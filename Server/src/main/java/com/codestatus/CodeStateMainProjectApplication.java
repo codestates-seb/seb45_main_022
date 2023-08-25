0948794e7252fda8e83e83424ee56a5b4a917a23
Island-Fever <handoseok@gmail.com>
2023-08-25 16:17:13 +0900

    ✨ feat: 백엔드 기본 세팅

---
+++ Server/src/main/java/com/codestatus/CodeStateMainProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStateMainProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeStateMainProjectApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/codestatus/CodeStateMainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeStateMainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
