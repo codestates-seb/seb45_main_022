61516cd4b0425547f342ae7aa5509e095d45f1e9
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-30 01:05:42 +0900

    ✨feat : 유효성 검사 추가

--- Client/src/components/user/Register.tsx
+++ Client/src/components/user/Register.tsx
@@ -8,8 +8,49 @@ const Register = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [nickname, setNickname] = useState<string>('');
-
+  const [emailErr, setEmailErr] = useState<boolean>(false);
+  const [passwordErr, setPasswordErr] = useState<boolean>(false);
+  const [nicknameErr, setNicknameErr] = useState<boolean>(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
+
+  // 이메일 포멧
+  const emailRegEx =
+    /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+  // 최소 8 자, 최소 하나의 문자, 하나의 숫자 및 하나의 특수 문자 :
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$/;
+
+  const nicknameRegex = /^[가-힣a-zA-Z]{2,6}$/;
+
+  const validateEmail = () => {
+    if (!emailRegEx.test(email)) {
+      setEmailErr(true);
+      console.log('email failure');
+    } else {
+      setEmailErr(false);
+      console.log('email success');
+    }
+  };
+
+  const validatePass = () => {
+    if (!passwordRegex.test(password)) {
+      setPasswordErr(true);
+      console.log('password failure');
+    } else {
+      setPasswordErr(false);
+      console.log('password success');
+    }
+  };
+
+  const validateNickname = () => {
+    if (!nicknameRegex.test(nickname)) {
+      setNicknameErr(true);
+      console.log('nickname failure');
+    } else {
+      setNicknameErr(false);
+      console.log('nickname success');
+    }
+  };
 
   const toggleViewPassword = () => {
     setPasswordVisible(!passwordVisible);
@@ -17,29 +58,39 @@ const Register = () => {
 
   const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    validateEmail();
+    validatePass();
+    validateNickname();
   };
   return (
     <div className="flex justify-center items-center">
-      <ModalFrame height={500} width={650}>
+      <ModalFrame height={550} width={700}>
         <form
           onSubmit={handleRegister}
-          className="flex flex-col items-center justify-center w-[500px] h-[380px] p-5"
+          className="flex flex-col items-center justify-center w-[550px] h-[450px] p-5"
         >
+          <h1 className="my-2 text-xl">Register</h1>
           <div className="flex items-center  ">
             <input
               placeholder="Email"
               type="text"
-              className=" border-solid border-2 border-000 p-2 rounded-lg my-2 "
+              className=" border-solid border-2 border-000 p-2 rounded-lg my-4 "
               onChange={(e) => setEmail(e.target.value)}
               value={email}
             />
           </div>
 
+          {emailErr && (
+            <p className="text-[10px]  text-red-500">
+              Please check email format
+            </p>
+          )}
+
           <div className="flex items-center relative">
             <input
               placeholder="Password"
               type={passwordVisible ? 'text' : 'password'}
-              className="border-solid border-2 border-000 p-2 rounded-lg my-2 w-full "
+              className="border-solid border-2 border-000 p-2 rounded-lg my-4 w-full "
               onChange={(e) => {
                 setPassword(e.target.value);
                 console.log(e.target.value);
@@ -54,7 +105,11 @@ const Register = () => {
               className="absolute right-2 cursor-pointer"
             />
           </div>
-
+          {passwordErr && (
+            <p className="text-[10px]  text-red-500">
+              8-16 characters, lower&upper case, numbers, and characters
+            </p>
+          )}
           {/* <div className="flex items-center">
             <input
               placeholder="Password Confirm"
@@ -66,15 +121,24 @@ const Register = () => {
             <input
               placeholder="Nickname "
               type="text"
-              className="border-solid border-2 border-000 p-2 rounded-lg my-2"
+              className="border-solid border-2 border-000 p-2 rounded-lg my-4"
               onChange={(e) => setNickname(e.target.value)}
               value={nickname}
             />
           </div>
 
-          <div className="my-4 text-sm">
+          {nicknameErr && (
+            <p className="text-[10px]  text-red-500">
+              2-6 characters, Korean or English
+            </p>
+          )}
+
+          <button className="w-[400px] h-[40px] bg-black text-white rounded-lg my-2 hover:cursor-grabbing">
+            Create an Account
+          </button>
+          {/* <div className="my-4 text-sm">
             <Button width={300}>Create an Account</Button>
-          </div>
+          </div> */}
 
           <div className="text-[10px] flex items-center justify-center w-full my-2">
             <span className="text-neutral-500">Already a user?</span>
