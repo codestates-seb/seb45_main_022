85ad929e89955e5621a3fabac47c39713032a738
DongHun, Kim <donghun.kdh@gmail.com>
2023-08-29 16:46:30 +0900

    ✨ feat: 종이 아이콘 클릭 시 프로필 창 표시

--- Client/src/components/title-page/StatusScreen.tsx
+++ Client/src/components/title-page/StatusScreen.tsx
@@ -9,7 +9,7 @@ const TitleScreen = ({ onClick }: Props) => {
   return (
     <Backdrop>
       <div className="relative w-full h-full flex flex-col justify-center items-center gap-[2rem]">
-        <div className="w-[1200px] h-[600px] bg-[url('/src/assets/common/modal-frame-note.png')] flex flex-row">
+        <div className="w-[1200px] h-[600px] bg-[url('/src/assets/common/modal-frame-note.png')] bg-center bg-cover bg-no-repeat flex flex-row">
           <div className="w-[600px] h-[600px]"></div>
           <div className="w-[600px] h-[600px] flex flex-col justify-center items-center"></div>
         </div>
--- Client/src/pages/TitlePage.tsx
+++ Client/src/pages/TitlePage.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import TitleScreen from '../components/title-page/TitleScreen';
 import StatusScreen from '../components/title-page/StatusScreen';
+import ProfileScreen from '../components/title-page/ProfileScreen';
 
 const TitlePage = () => {
   const [showTitle, setShowTitle] = useState<boolean>(true);
   const [showStatus, setShowStatus] = useState<boolean>(false);
+  const [showProfile, setShowProfile] = useState<boolean>(false);
   return (
     <>
       <div className="w-full h-screen flex justify-center items-center bg-[#3c0033] z-10">
@@ -26,16 +28,40 @@ const TitlePage = () => {
               }}
             />
           )}
+          {showProfile && (
+            <ProfileScreen
+              onClick={() => {
+                setShowProfile(false);
+              }}
+            />
+          )}
           {!showTitle && (
-            <div className="absolute left-[400px] top-[360px] flex flex-col justify-between items-center gap-[1rem]">
-              <p className="text-white drop-shadow-[0_0_2px_#000]">My Status</p>
-              <div
-                className=" w-[60px] h-[60px] bg-[url('/src/assets/objects/note.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-150 hover:scale-105 cursor-pointer"
-                onClick={() => {
-                  setShowStatus(true);
-                }}
-              ></div>
-            </div>
+            <>
+              {/* 책 아이콘 */}
+              <div className="absolute left-[400px] top-[360px] flex flex-col justify-between items-center gap-[1rem]">
+                <p className="text-white drop-shadow-[0_0_2px_#000]">
+                  My Status
+                </p>
+                <div
+                  className=" w-[60px] h-[60px] bg-[url('/src/assets/objects/note.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-150 hover:scale-105 cursor-pointer"
+                  onClick={() => {
+                    setShowStatus(true);
+                  }}
+                ></div>
+              </div>
+              {/* 종이 아이콘 */}
+              <div className="absolute left-[745px] top-[240px] flex flex-col justify-between items-center gap-[1rem]">
+                <p className="text-white drop-shadow-[0_0_2px_#000]">
+                  My Profile
+                </p>
+                <div
+                  className=" w-[50px] h-[60px] bg-[url('/src/assets/objects/paper.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-150 hover:scale-105 cursor-pointer"
+                  onClick={() => {
+                    setShowProfile(true);
+                  }}
+                ></div>
+              </div>
+            </>
           )}
         </div>
       </div>
