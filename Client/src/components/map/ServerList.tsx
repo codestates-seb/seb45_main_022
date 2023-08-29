ab016fadec553c4eccab09170aafbd920da2d57c
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-08-29 22:22:51 +0900

    💄 design: icon 추가 및 Backdrop & Button 컴포넌트 적용

--- Client/src/components/map/ServerList.tsx
+++ Client/src/components/map/ServerList.tsx
@@ -1,13 +1,30 @@
+import Button from '../common/Button';
+import weightIcon from '../../assets/icons/weight.png';
+import joggingIcon from '../../assets/icons/jogging.png';
+
 interface Props {
   title: string;
-  numberOfPosts: string;
 }
 
-export const ServerList = ({ title, numberOfPosts }: Props) => {
+const ServerList = ({ title }: Props) => {
+  let icon;
+  if (title === 'Weight Training') {
+    icon = weightIcon;
+  } else if (title === 'Running') {
+    icon = joggingIcon;
+  }
+
   return (
-    <div className="flex flex-col justify-center items-center gap-4 cursor-pointer">
-      <div className="text-xl font-bold">{title}</div>
-      <div className="text-gray-600">{numberOfPosts}</div>
+    <div className="flex w-[100%] justify-between items-center cursor-pointer">
+      <div className="flex w-[150px] justify-center items-center">
+        <img className="width" src={icon} alt="icon" />
+      </div>
+      <div className="flex w-[50%] flex-col justify-center items-center gap-3">
+        <div className="text-xl font-bold">{title}</div>
+        <Button>Stat Up !</Button>
+      </div>
     </div>
   );
 };
+
+export default ServerList;
--- Client/src/pages/MapPage.tsx
+++ Client/src/pages/MapPage.tsx
@@ -1,19 +1,22 @@
+import Backdrop from '../components/common/Backdrop';
 import ModalFrame from '../components/common/ModalFrame';
-import { ServerList } from '../components/map/ServerList';
+import ServerList from '../components/map/ServerList';
 
 const MapPage = () => {
   return (
     <div className="flex justify-center items-center w-full h-full absolute bg-map bg-center bg-cover">
-      <ModalFrame width={1020} height={680}>
-        <div className="w-[800px] h-[80px] flex justify-center items-center text-4xl mb-4">
-          Servers
-        </div>
-        <div className="w-full h-px bg-gray-600"></div>
-        <div className="w-[800px] h-[480px] flex flex-col justify-around items-center p-10 gap-4">
-          <ServerList title="Weight Training" numberOfPosts="5 posts" />
-          <ServerList title="Running" numberOfPosts="3 posts" />
-        </div>
-      </ModalFrame>
+      <Backdrop>
+        <ModalFrame width={1020} height={680}>
+          <div className="w-[800px] h-[80px] flex justify-center items-center text-4xl mb-4">
+            Servers
+          </div>
+          <div className="w-full h-px bg-gray-600"></div>
+          <div className="w-[800px] h-[480px] flex flex-col justify-around items-center p-10 gap-4">
+            <ServerList title="Weight Training" />
+            <ServerList title="Running" />
+          </div>
+        </ModalFrame>
+      </Backdrop>
     </div>
   );
 };
