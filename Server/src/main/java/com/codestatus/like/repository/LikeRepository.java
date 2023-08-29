c9d956e129b53ef6ce3f18455df05892a73e99f1
jjo3ys <jjo3ys@gmail.com>
2023-08-29 15:10:31 +0900

    ✨  feat:like repository 추가

--- Server/src/main/java/com/codestatus/like/repository/LikeRepository.java
+++ Server/src/main/java/com/codestatus/like/repository/LikeRepository.java
@@ -1,4 +1,10 @@
 package com.codestatus.like.repository;
 
-public interface LikeRepository {
+import com.codestatus.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findLikeByFeed_FeedIdAndUser_UserId(long feedId, long userId);
 }
