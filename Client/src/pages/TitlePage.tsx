78c4e8dec1294a2cd3800f692afbfd4c0e072c91
DongHun, Kim <donghun.kdh@gmail.com>
2023-08-30 00:54:08 +0900

    ⚙️ chore: src/components/title-page > src/components/title로 이름 변경; 유저 정보 조회 Mock data 추가

--- Client/src/pages/TitlePage.tsx
+++ Client/src/pages/TitlePage.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import TitleScreen from '../components/title-page/TitleScreen';
-import StatusScreen from '../components/title-page/StatusScreen';
-import ProfileScreen from '../components/title-page/ProfileScreen';
+import TitleScreen from '../components/title/TitleScreen';
+import StatusScreen from '../components/title/StatusScreen';
+import ProfileScreen from '../components/title/ProfileScreen';
 
 const TitlePage = () => {
   const [showTitle, setShowTitle] = useState<boolean>(true);
@@ -38,24 +38,24 @@ const TitlePage = () => {
           {!showTitle && (
             <>
               {/* 책 아이콘 */}
-              <div className="absolute left-[400px] top-[360px] flex flex-col justify-between items-center gap-[1rem]">
-                <p className="text-white drop-shadow-[0_0_2px_#000]">
+              <div className="absolute left-[410px] top-[360px] flex flex-col justify-between items-center gap-[1rem]">
+                <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
                   My Status
                 </p>
                 <div
-                  className=" w-[60px] h-[60px] bg-[url('/src/assets/objects/note.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-150 hover:scale-105 cursor-pointer"
+                  className=" w-[60px] h-[60px] bg-[url('/src/assets/objects/note.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"
                   onClick={() => {
                     setShowStatus(true);
                   }}
                 ></div>
               </div>
               {/* 종이 아이콘 */}
-              <div className="absolute left-[745px] top-[240px] flex flex-col justify-between items-center gap-[1rem]">
-                <p className="text-white drop-shadow-[0_0_2px_#000]">
+              <div className="absolute left-[755px] top-[240px] flex flex-col justify-between items-center gap-[1rem]">
+                <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
                   My Profile
                 </p>
                 <div
-                  className=" w-[50px] h-[60px] bg-[url('/src/assets/objects/paper.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-150 hover:scale-105 cursor-pointer"
+                  className=" w-[50px] h-[60px] bg-[url('/src/assets/objects/paper.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"
                   onClick={() => {
                     setShowProfile(true);
                   }}
