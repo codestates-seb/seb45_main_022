ec245235757d25344ea3a4c79bf1c11307806778
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-30 00:27:19 +0900

    ✨feat : 유저 정보 상태관리

--- Client/src/components/user/Register.tsx
+++ Client/src/components/user/Register.tsx
@@ -5,20 +5,33 @@ import view from '../../assets/icons/view.png';
 import { useState } from 'react';
 
 const Register = () => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [nickname, setNickname] = useState<string>('');
+
   const [passwordVisible, setPasswordVisible] = useState(false);
 
   const toggleViewPassword = () => {
     setPasswordVisible(!passwordVisible);
   };
+
+  const handleRegister = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <div className="flex justify-center items-center">
       <ModalFrame height={500} width={650}>
-        <form className="flex flex-col items-center justify-center w-[500px] h-[380px] p-5">
+        <form
+          onSubmit={handleRegister}
+          className="flex flex-col items-center justify-center w-[500px] h-[380px] p-5"
+        >
           <div className="flex items-center  ">
             <input
               placeholder="Email"
               type="text"
               className=" border-solid border-2 border-000 p-2 rounded-lg my-2 "
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
             />
           </div>
 
@@ -27,6 +40,11 @@ const Register = () => {
               placeholder="Password"
               type={passwordVisible ? 'text' : 'password'}
               className="border-solid border-2 border-000 p-2 rounded-lg my-2 w-full "
+              onChange={(e) => {
+                setPassword(e.target.value);
+                console.log(e.target.value);
+              }}
+              value={password}
             />
             <img
               src={!passwordVisible ? hide : view}
@@ -49,6 +67,8 @@ const Register = () => {
               placeholder="Nickname "
               type="text"
               className="border-solid border-2 border-000 p-2 rounded-lg my-2"
+              onChange={(e) => setNickname(e.target.value)}
+              value={nickname}
             />
           </div>
 
