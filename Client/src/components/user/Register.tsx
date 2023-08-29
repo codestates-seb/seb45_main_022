c0bb8c1c123ef76521e32afc04353cb27655a26c
Jiwon Lee <giwon.yi339@gmail.com>
2023-08-30 00:07:21 +0900

    ✨feat : 비밀번호 보기/숨기기 기능 구현

--- Client/src/components/user/Register.tsx
+++ Client/src/components/user/Register.tsx
@@ -1,7 +1,15 @@
 import ModalFrame from '../common/ModalFrame';
 import Button from '../common/Button';
+import hide from '../../assets/icons/hide.png';
+import view from '../../assets/icons/view.png';
+import { useState } from 'react';
 
 const Register = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const toggleViewPassword = () => {
+    setPasswordVisible(!passwordVisible);
+  };
   return (
     <div className="flex justify-center items-center">
       <ModalFrame height={500} width={650}>
@@ -17,14 +25,34 @@ const Register = () => {
           <div className="flex items-center">
             <input
               placeholder="Password"
+              type={passwordVisible ? 'text' : 'password'}
+              className="border-solid border-2 border-000 p-2 rounded-lg my-2"
+            />
+            <img
+              src={view}
+              alt="view icon"
+              width={35}
+              onClick={toggleViewPassword}
+            />
+          </div>
+          {/* <div className="flex items-center">
+            <input
+              placeholder="Password Confirm"
               type="password"
+              className="border-solid border-2 border-000 p-2 rounded-lg my-2"
+            />
+          </div> */}
+          <div className="flex items-center">
+            <input
+              placeholder="Nickname "
+              type="text"
               className="border-solid border-2 border-000 p-2 rounded-lg my-2"
             />
           </div>
 
-          <Button width={300}>
-            <span>Create an Account</span>
-          </Button>
+          <div className="my-2 text-sm">
+            <Button width={300}>Create an Account</Button>
+          </div>
         </form>
       </ModalFrame>
     </div>
