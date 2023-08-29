aee5f3b85c8cd4f51d3dd3373404893067a29c98
jjo3ys <jjo3ys@gmail.com>
2023-08-29 13:27:58 +0900

    ✨  feat:comment service cud 기능 추가

--- Server/src/main/java/com/codestatus/comment/service/CommentService.java
+++ Server/src/main/java/com/codestatus/comment/service/CommentService.java
@@ -1,4 +1,53 @@
 package com.codestatus.comment.service;
 
+import com.codestatus.comment.entity.Comment;
+import com.codestatus.comment.repository.CommentRepository;
+import com.codestatus.exception.BusinessLogicException;
+import com.codestatus.exception.ExceptionCode;
+import com.codestatus.feed.entity.Feed;
+import com.codestatus.feed.mapper.FeedMapper;
+import com.codestatus.user.entity.User;
+import com.codestatus.user.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Transactional
+@Service
 public class CommentService {
+    private final CommentRepository commentRepository;
+    private final UserMapper userMapper;
+    private final FeedMapper feedMapper;
+    public CommentService(CommentRepository commentRepository, UserMapper userMapper, FeedMapper feedMapper) {
+        this.commentRepository = commentRepository;
+        this.userMapper = userMapper;
+        this.feedMapper = feedMapper;
+    }
+
+    public void createComment(long feedId, long userId, Comment comment){
+        User user = userMapper.userIdToUser(userId);
+        Feed feed = feedMapper.feedIdToFeed(feedId);
+        commentRepository.save(comment);
+    }
+
+    public void updateComment(long commentId, Comment comment){
+        Comment findComment = findVerifiedComment(commentId);
+        Optional.ofNullable(comment.getBody()).ifPresent(findComment::setBody);
+
+        commentRepository.save(comment);
+    }
+
+    public void deleteComment(long commentId) {
+        Comment findComment = findVerifiedComment(commentId);
+        findComment.setDeleted(true);
+
+        commentRepository.save(findComment);
+    }
+
+    @Transactional(readOnly = true)
+    public Comment findVerifiedComment(long commentId){
+        Optional<Comment> optionalComment = commentRepository.findCommentByCommentIdAndDeleted(commentId, false);
+        return optionalComment.orElseThrow(() -> new BusinessLogicException(ExceptionCode.COMMENT_NOT_FOUND));
+    }
 }
