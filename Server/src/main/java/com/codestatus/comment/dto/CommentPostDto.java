3d05c9b6afdafba3034d22499ddeb761ab0eff15
jjo3ys <jjo3ys@gmail.com>
2023-08-29 13:46:28 +0900

    ✨  feat:body validation 추가

--- Server/src/main/java/com/codestatus/comment/dto/CommentPostDto.java
+++ Server/src/main/java/com/codestatus/comment/dto/CommentPostDto.java
@@ -3,8 +3,11 @@ package com.codestatus.comment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @AllArgsConstructor
 public class CommentPostDto {
+    @NotEmpty
     String body;
 }
--- Server/src/main/java/com/codestatus/comment/dto/CommnetPatchDto.java
+++ Server/src/main/java/com/codestatus/comment/dto/CommnetPatchDto.java
@@ -3,8 +3,11 @@ package com.codestatus.comment.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @AllArgsConstructor
 public class CommnetPatchDto {
+    @NotEmpty
     String body;
 }
