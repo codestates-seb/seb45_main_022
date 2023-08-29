4f0e91a3cdb6da308512aef144c1ea51c01b7792
DongHun, Kim <donghun.kdh@gmail.com>
2023-08-29 16:04:45 +0900

    ♻️ refactor: TitleScreen 컨텐츠를 Backdrop이 감싸도록 수정

--- Client/src/components/title-page/TitleScreen.tsx
+++ Client/src/components/title-page/TitleScreen.tsx
@@ -6,18 +6,19 @@ interface Props {
 
 const TitleScreen = ({ onClick }: Props) => {
   return (
-    <div
-      className="w-full h-full flex flex-col justify-center items-center gap-[15rem]"
-      onClick={onClick}
-    >
-      <Backdrop />
-      <h1 className="text-white font-extrabold font-default text-[8rem] tracking-wide drop-shadow-[10px_10px_2px_#000] z-10 pointer-events-none">
-        STAT & US
-      </h1>
-      <p className="text-white text-[2rem] z-10 animate-[blink_.8s_alternate_infinite] pointer-events-none">
-        CLICK ANYWHERE TO ACCESS
-      </p>
-    </div>
+    <Backdrop>
+      <div
+        className="w-full h-full flex flex-col justify-center items-center gap-[15rem]"
+        onClick={onClick}
+      >
+        <h1 className="text-white font-extrabold font-default text-[8rem] tracking-wide drop-shadow-[10px_10px_2px_#000] pointer-events-none">
+          STAT & US
+        </h1>
+        <p className="text-white text-[2rem] animate-[blink_.8s_alternate_infinite] pointer-events-none">
+          CLICK ANYWHERE TO ACCESS
+        </p>
+      </div>
+    </Backdrop>
   );
 };
 
