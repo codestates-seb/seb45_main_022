1fc4e90a9ad5c94717da112210647044f33c023e
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-08-30 15:47:35 +0900

    ✨ feat: 카테고리 조회 시, map/test 경로일 때만 mock data 사용

--- Client/src/components/map/ServerList.tsx
+++ Client/src/components/map/ServerList.tsx
@@ -1,23 +1,18 @@
 import Button from '../common/Button';
-import weightIcon from '../../assets/icons/weight.png';
-import joggingIcon from '../../assets/icons/jogging.png';
+import { icons } from '../../utility/icon';
 
 interface Props {
   title: string;
+  categoryCode: number;
 }
 
-const ServerList = ({ title }: Props) => {
-  let icon;
-  if (title === 'Weight Training') {
-    icon = weightIcon;
-  } else if (title === 'Running') {
-    icon = joggingIcon;
-  }
+const ServerList = ({ title, categoryCode }: Props) => {
+  const icon = icons[categoryCode];
 
   return (
     <div className="flex w-[100%] justify-between items-center cursor-pointer">
       <div className="flex w-[150px] justify-center items-center">
-        <img className="width" src={icon} alt="icon" />
+        {icon && <img className="width" src={icon} alt="icon" />}
       </div>
       <div className="flex w-[50%] flex-col justify-center items-center gap-3">
         <div className="text-xl font-bold">{title}</div>
--- Client/src/pages/MapPage.tsx
+++ Client/src/pages/MapPage.tsx
@@ -1,8 +1,67 @@
+//MapPage.tsx
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 import Backdrop from '../components/common/Backdrop';
 import ModalFrame from '../components/common/ModalFrame';
 import ServerList from '../components/map/ServerList';
+import axios from 'axios';
+
+interface Category {
+  categoryCode: number;
+  categoryName: string;
+}
+
+interface JsonData {
+  statusCode: number;
+  statusName: string;
+  categories: Category[];
+}
 
 const MapPage = () => {
+  const [categoryList, setCategoryList] = useState<string[]>([]);
+  const [iconNumbers, setIconNumbers] = useState<number[]>([]);
+  const { statusCode } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<JsonData>(
+          `../../public/categoryList/${statusCode}.json`,
+        );
+        const jsonData = await response.data;
+
+        const selectedCategoryCodes = jsonData.categories.map(
+          (category) => category.categoryCode,
+        );
+
+        const selectedCategoryNames = selectedCategoryCodes
+          .filter((code) =>
+            jsonData.categories.some(
+              (category) => category.categoryCode === code,
+            ),
+          )
+          .map((code) => {
+            const selectedCategory = jsonData.categories.find(
+              (category) => category.categoryCode === code,
+            );
+
+            return selectedCategory ? selectedCategory.categoryName : '';
+          });
+
+        const selectedIconNumbers = selectedCategoryCodes.map(
+          (code) => code % 13, // 각각의 카테고리에 대한 끝자리 추출
+        );
+
+        setCategoryList(selectedCategoryNames);
+        setIconNumbers(selectedIconNumbers);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [statusCode]);
+
   return (
     <div className="flex justify-center items-center w-full h-full absolute bg-map bg-center bg-cover">
       <Backdrop>
@@ -12,8 +71,13 @@ const MapPage = () => {
           </div>
           <div className="w-full h-px bg-gray-600"></div>
           <div className="w-[800px] h-[480px] flex flex-col justify-around items-center p-10 gap-4">
-            <ServerList title="Weight Training" />
-            <ServerList title="Running" />
+            {categoryList.map((categoryName, index) => (
+              <ServerList
+                key={index}
+                title={categoryName}
+                categoryCode={iconNumbers[index]}
+              />
+            ))}
           </div>
         </ModalFrame>
       </Backdrop>
