3970c7a94f546684059e88c73400185907dc8a73
DongHun, Kim <donghun.kdh@gmail.com>
2023-08-31 18:10:55 +0900

    ✨ feat: Button 컴포넌트 width, height 대신 size를 props로 넘겨주도록 수정

--- Client/src/components/common/Button.tsx
+++ Client/src/components/common/Button.tsx
@@ -1,18 +1,34 @@
 interface Props {
-  width?: number;
-  height?: number;
+  size?: 'small' | 'medium' | 'large';
   children?: React.ReactNode;
   onClick?: () => void;
 }
 
-const Button = ({ width = 200, height = 50, children, onClick }: Props) => {
+const Button = ({ size = 'large', children, onClick }: Props) => {
+  const width = {
+    small: 120,
+    medium: 160,
+    large: 200,
+  };
+  const height = {
+    small: 30,
+    medium: 40,
+    large: 50,
+  };
+  const fontSize = {
+    small: '.6rem',
+    medium: '.8rem',
+    large: '1rem',
+  };
+
   return (
     <div
       style={{
-        width: `${width}px`,
-        height: `${height}px`,
+        width: `${width[size]}px`,
+        height: `${height[size]}px`,
+        fontSize: `${fontSize[size]}`,
       }}
-      className="bg-[url('/src/assets/common/button.png')] bg-cover bg-no-repeat bg-center flex justify-center items-center hover:brightness-110 duration-300 cursor-pointer"
+      className="bg-[url('/src/assets/common/button.png')] bg-cover bg-no-repeat bg-center flex justify-center items-center hover:brightness-110 duration-300 cursor-pointer text-center"
       onClick={onClick}
     >
       {children}
