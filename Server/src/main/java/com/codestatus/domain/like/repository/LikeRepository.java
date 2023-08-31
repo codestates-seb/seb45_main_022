a26e60cebc41923da7008c7321f442937d463f0a
jjo3ys <jjo3ys@gmail.com>
2023-08-31 14:24:28 +0900

    ✨  feat: first like gain exp 기능 추가

--- Server/src/main/java/com/codestatus/domain/like/repository/LikeRepository.java
+++ Server/src/main/java/com/codestatus/domain/like/repository/LikeRepository.java
@@ -1,10 +1,12 @@
 package com.codestatus.domain.like.repository;
 
+import com.codestatus.domain.feed.entity.Feed;
 import com.codestatus.domain.like.entity.Like;
+import com.codestatus.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    Optional<Like> findLikeByFeed_FeedIdAndUser_UserId(long feedId, long userId);
+    Optional<Like> findLikeByFeedAndUser(Feed feed, User user);
 }
--- Server/src/main/java/com/codestatus/domain/like/service/LikeService.java
+++ Server/src/main/java/com/codestatus/domain/like/service/LikeService.java
@@ -1,13 +1,15 @@
 package com.codestatus.domain.like.service;
 
+import com.codestatus.domain.feed.service.FeedService;
 import com.codestatus.domain.like.repository.LikeRepository;
 import com.codestatus.domain.feed.entity.Feed;
-import com.codestatus.domain.feed.mapper.FeedMapper;
 import com.codestatus.domain.like.entity.Like;
 import com.codestatus.domain.user.entity.User;
-import com.codestatus.domain.user.mapper.UserMapper;
 import com.codestatus.domain.user.service.UserService;
+import com.codestatus.global.exception.BusinessLogicException;
+import com.codestatus.global.exception.ExceptionCode;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -16,33 +18,35 @@ import java.util.Optional;
 @Service
 public class LikeService {
     private final UserService userService;
-    private final UserMapper userMapper;
+    private final FeedService feedService;
     private final LikeRepository likeRepository;
-    private final FeedMapper feedMapper;
 
-    public LikeService(UserService userService, UserMapper userMapper, LikeRepository likeRepository, FeedMapper feedMapper) {
+    public LikeService(UserService userService, FeedService feedService, LikeRepository likeRepository) {
         this.userService = userService;
-        this.userMapper = userMapper;
+        this.feedService = feedService;
         this.likeRepository = likeRepository;
-        this.feedMapper = feedMapper;
     }
 
     public void feedLikeOrDisLike(long feedId, long userId){
-        userService.findVerifiedUser(userId);
-        // feed 유효성 검사 feed 수정 시 구현 예정
-        // 자추 불가 기능 feed service 구현 시 추가 예정
-        Optional<Like> optionalLike = likeRepository.findLikeByFeed_FeedIdAndUser_UserId(feedId, userId);
+        // user, feed 유효성 검사
+        User user = userService.findVerifiedUser(userId);
+        Feed feed = feedService.findVerifiedFeed(feedId);
+        // 자추 불가
+        if (feed.getUser().getUserId() == userId) throw new BusinessLogicException(ExceptionCode.LIKE_BAD_REQUEST);
+
+        Optional<Like> optionalLike = likeRepository.findLikeByFeedAndUser(feed, user);
         Like like;
+        // 좋아요 존재 시 현재 deleted 상태를 반대로 바꿈
         if (optionalLike.isPresent()) {
             like = optionalLike.get();
             like.setDeleted(!like.getDeleted());
         } else {
+            // 좋아요 없으면 새로 생성하고, 피드 주인 경험치 획득하고 레벨업 검사
             like = new Like();
-            Feed feed = feedMapper.feedIdToFeed(feedId);
-            User user = userMapper.userIdToUser(userId);
             like.setFeed(feed);
             like.setUser(user);
-            // 경험치 획득 메서드 feed category entity 수정 시 구현 예정
+            // 경험치 획득 및 level up  check 메서드
+            userService.gainExp(user, 10, feed.getCategory().getStat().getStatId().intValue());
         }
         likeRepository.save(like);
     }
