10725e4be1e1887336c7754c7dac107c0187692f
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-01 19:41:45 +0900

    💄 design: 필터 버튼 디자인 변경 및 클릭 여부에 따른 배경색 변경

--- Client/src/components/feed/FilterButton.tsx
+++ Client/src/components/feed/FilterButton.tsx
@@ -1,14 +1,45 @@
-import ModalFrame from '../common/ModalFrame';
+import { useState } from 'react';
 
-const FilterButton = () => {
+const FilterButton: React.FC = () => {
+  const [clicked, setClicked] = useState<string>('최신순');
+
+  const buttonStyle: React.CSSProperties = {
+    fontFamily: 'Pretendard',
+    padding: '0.5rem 1rem',
+    borderRadius: '0.25rem',
+    cursor: 'pointer',
+  };
+
+  const clickedButtonStyle: React.CSSProperties = {
+    ...buttonStyle,
+    backgroundColor: '#fcebd7',
+  };
+
+  const unclickedButtonStyle: React.CSSProperties = {
+    ...buttonStyle,
+    backgroundColor: '#e8cead',
+  };
+
+  const handleButtonClick = (buttonText: string) => {
+    setClicked(buttonText);
+  };
+
   return (
-    <div className=" flex justify-center items-center gap-3">
-      <ModalFrame width={70} height={50}>
+    <div className="flex justify-center items-center gap-5">
+      <button
+        style={clicked === '최신순' ? clickedButtonStyle : unclickedButtonStyle}
+        onClick={() => handleButtonClick('최신순')}
+      >
         최신순
-      </ModalFrame>
-      <ModalFrame width={70} height={50}>
-        베스트
-      </ModalFrame>
+      </button>
+      <button
+        style={
+          clicked === '주간 베스트' ? clickedButtonStyle : unclickedButtonStyle
+        }
+        onClick={() => handleButtonClick('주간 베스트')}
+      >
+        주간 베스트
+      </button>
     </div>
   );
 };
