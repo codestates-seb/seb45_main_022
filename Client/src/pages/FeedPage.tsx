265438d1c1cf84ef33a2a444b8f7b4080a404003
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-03 18:36:52 +0900

    ✨ feat: Mock 데이터를 노출하는 피드 페이지 구현

--- Client/src/pages/FeedPage.tsx
+++ Client/src/pages/FeedPage.tsx
@@ -1,5 +1,25 @@
+import Backdrop from '../components/common/Backdrop';
+import Header from '../components/feed/Header';
+import board from '../assets/feed/board.png';
+import Main from '../components/feed/Main';
+
 const FeedPage = () => {
-  return <div>FeedPage</div>;
+  const sectionStyle = {
+    backgroundImage: `url(${board})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen bg-feed bg-center bg-cover">
+      <Backdrop>
+        <div className=" w-[1080px] h-[720px] p-[42px]" style={sectionStyle}>
+          <Header />
+          <Main />
+        </div>
+      </Backdrop>
+    </div>
+  );
 };
 
 export default FeedPage;
