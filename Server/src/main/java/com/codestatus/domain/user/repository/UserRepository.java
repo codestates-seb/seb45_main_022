09cd94e1678db004420ecd6008b7d6a0278ebd73
Island-Fever <handoseok@gmail.com>
2023-09-04 19:17:33 +0900

    📝 docs: UserRepository 쿼리문 추가

--- Server/src/main/java/com/codestatus/domain/user/repository/UserRepository.java
+++ Server/src/main/java/com/codestatus/domain/user/repository/UserRepository.java
@@ -2,12 +2,27 @@ package com.codestatus.domain.user.repository;
 
 import com.codestatus.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByNickName(String nickName);
-    List<User> findAllByUserStatusAndAttendance(User.UserStatus userStatus, boolean b);
+    // @Query(value = "SELECT * FROM users u WHERE u.email = :email", nativeQuery = true)
+    Optional<User> findByEmail(String email); // @Param("email")
+
+    // @Query(value = "SELECT * FROM users u WHERE u.nick_name = :nickname", nativeQuery = true)
+    Optional<User> findByNickName(String nickName); // @Param("nickname")
+
+    // @Query(value = "SELECT * FROM users u WHERE u.user_status = :userStatus AND u.attendance = :attendance", nativeQuery = true)
+    List<User> findAllByUserStatusAndAttendance(User.UserStatus userStatus, boolean b); // @Param("userStatus") @Param("attendance")
 }
+
+/*
+* SELECT * FROM users WHERE email = 'signup@test.luv';
+* SELECT * FROM users WHERE nick_name = '테스터';
+* SELECT * FROM users
+* WHERE user_status = 'USER_ACTIVE' // or 'USER_DELETE'
+* AND attendance = FALSE;
+ */
