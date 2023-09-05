9aa3e52d66c2510996289d94f2414e15ad7548f2
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-05 09:42:08 +0900

    ✨feat : 댓글창 레이아웃 <li> 요소러 수정해서, 정상적으로 리스트업 됨

--- Client/src/components/map/UserPost.tsx
+++ Client/src/components/map/UserPost.tsx
@@ -8,21 +8,66 @@ import icon from '../../assets/icons/weight.png';
 interface Comment {
   id: number;
   nickname: string;
+  label: string;
   text: string;
 }
 
 const UserPost = () => {
-  const [newComment, setNewComment] = useState('');
   const [comments, setComments] = useState<Comment[]>([
-    { id: 1, nickname: '하이', text: '클럽 땡기네 ㅋㅋ 헬스클럽' },
-    { id: 2, nickname: '흑룡', text: '나 3대 500임' },
+    {
+      id: 1,
+      nickname: '하이',
+      label: 'Lv. 01',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 2,
+      nickname: '바이',
+      label: 'Lv. 03',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, consequuntur?',
+    },
     {
       id: 3,
-      nickname: '흑룡',
-      text: '나 3대 500임 Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ratione ipsum et placeat inventore ut excepturi amet quia minima reiciendis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, recusandae incidunt? Doloremque voluptatum consequatur ab aliquam. Illo inventore adipisci odio',
+      nickname: 'React',
+      label: 'Lv. 05',
+      text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, consequuntur?',
     },
-    { id: 4, nickname: '흑룡', text: '나 3대 500임' },
-    { id: 5, nickname: '흑룡', text: '나 3대 500임' },
+    {
+      id: 4,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 5,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 6,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 7,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 9,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
+    {
+      id: 9,
+      nickname: 'Redux',
+      label: 'Lv. 08',
+      text: '클럽 땡기네 ㅋㅋ 헬스클럽',
+    },
   ]);
   const [displayComments, setDisplayComments] = useState(3);
 
@@ -30,11 +75,15 @@ const UserPost = () => {
     setDisplayComments(displayComments + 3);
   };
 
+  const hideComments = () => {
+    setDisplayComments(3);
+  };
+
   return (
     // <PostFrame width={1100} height={550}>
-    <div className="p-4 flex flex-col items-center w-[900px] h-[700px] bg-[rgba(111,111,111,0.1)] border-4">
+    <div className="   p-4 flex flex-col items-center w-[900px] border border-solid border-black ">
       <div className="flex ">
-        <div className=" border-r-2 py-2 flex flex-col items-center justify-center w-[200px] h-[300px]  ">
+        <div className=" border border-solid border-black py-2 flex flex-col items-center justify-center w-[200px]  ">
           <img src={profileImg} width={90} alt="profile pic" className="mb-2" />
           <span className="font-[Pretendard]">동훈</span>
           <div className="flex items-center justify-around w-[100px]">
@@ -42,8 +91,8 @@ const UserPost = () => {
             <span className="font-[Pretendard]">Lv. 01</span>
           </div>
         </div>
-        <div className="flex flex-col items-center py-2 w-[600px] ">
-          <p className="p-6 font-[Pretendard] font-semibold ">
+        <div className="border border-solid border-black  flex flex-col items-center py-2 w-[600px] ">
+          <p className="font-[Pretendard] p-6 font-semibold ">
             솔직히 3대 200도 못들면 헬스 접는게 맞음 ㄹㅇ ㅋㅋ 벤치 40에 스쾃 80
             데드 80만 들어도 3대 ㅋㅋ
           </p>
@@ -54,43 +103,45 @@ const UserPost = () => {
           />
         </div>
       </div>
-      <div className="h-[280px]  w-[800px]  p-4 overflow-y-scroll">
-        <div className="   flex flex-col justify-evenly  ">
-          {comments.slice(0, displayComments).map((comment) => (
-            <div key={comment.id} className=" h-[35px] mb-1">
-              <span className="w-[180px]  text-xl mr-5 font-bold font-[Pretendard]">
-                {comment.nickname}
-              </span>
-              <span className="font-[Pretendard]">{comment.text}</span>
-            </div>
-          ))}
-        </div>
-      </div>
-      {comments.length > 3 && (
-        <div>
+      <div>
+        {comments.length > 3 && (
           <button
             onClick={expandComments}
             className="font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm"
           >
             View More Comments
-          </button>{' '}
-          <button className="font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm">
+          </button>
+        )}
+
+        {displayComments > 3 && (
+          <button
+            onClick={hideComments}
+            className="font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm"
+          >
             Hide Comments
-          </button>{' '}
+          </button>
+        )}
+      </div>
+      <div className="border border-solid border-black w-[800px] ">
+        <div className="   flex flex-col justify-evenly  ">
+          {comments.slice(0, displayComments).map((comment) => (
+            <div className="flex items-center justify-start p-2 mb-1  text-xl mr-5 font-bold">
+              <ul key={comment.id} className="">
+                <li className="font-[Pretendard]">
+                  {comment.nickname} ({comment.label}) : {comment.text}
+                </li>
+              </ul>
+            </div>
+          ))}
         </div>
-      )}
-      <div className="flex item-center m-auto">
-        <input
-          type="text"
-          className="font-[Pretendard] rounded-xl mt-1 w-[400px] h-[40px] border-2 "
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-        />
-        <button className="font-[Pretendard] ml-4 bg-white w-[100px] rounded-xl text-xm">
-          Submit
-        </button>
       </div>
     </div>
+
+    //     <span className="w-[180px]  text-xl mr-5 font-bold font-[Pretendard]">
+    //     {comment.nickname}
+    //   </span>
+    //   <span className="font-[Pretendard]">{comment.text}</span>
+    // </div>
 
     // </PostFrame>
   );
