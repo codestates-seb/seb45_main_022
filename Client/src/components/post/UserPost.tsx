31760151776494806b6471832a4f21104b04b84b
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-05 11:11:16 +0900

    💄 design: 피드 간격 (padding/margin) 수정 및 border-bottom 추가

--- Client/src/components/post/UserPost.tsx
+++ Client/src/components/post/UserPost.tsx
@@ -18,55 +18,63 @@ const UserPost = () => {
   const [comments, setComments] = useState<Comment[]>([
     {
       id: 1,
+      profileImg: profileImg,
       nickname: '하이',
       label: 'Lv. 01',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '1h',
+      timeCreated: '1H',
     },
     {
       id: 2,
+      profileImg: profileImg,
       nickname: '바이',
       label: 'Lv. 03',
-      text: 'Lorem ipsum dolor sit, amet cons ectetur adipisicing el ectetur adipisicing elit. Iusto, consequuntur?',
-      timeCreated: '5h',
+      text: 'Lorem ipsum dolor sit, psum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturpsum dolor sit, amet cons ectetur adipisicing el ecteturamet cons ectetur adipisicing el ectetur adipisicing elit. Iusto, consequuntur?',
+      timeCreated: '5H',
     },
     {
       id: 3,
+      profileImg: profileImg,
       nickname: 'React',
       label: 'Lv. 05',
       text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto, consequuntur?',
-      timeCreated: '1min',
+      timeCreated: '1M',
     },
     {
       id: 4,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '5min',
+      timeCreated: '5M',
     },
     {
       id: 5,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '2 day',
+      timeCreated: '2D',
     },
     {
       id: 6,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '3 day',
+      timeCreated: '3D',
     },
     {
       id: 7,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '12h',
+      timeCreated: '12H',
     },
     {
       id: 9,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
@@ -74,10 +82,11 @@ const UserPost = () => {
     },
     {
       id: 9,
+      profileImg: profileImg,
       nickname: 'Redux',
       label: 'Lv. 08',
       text: '클럽 땡기네 ㅋㅋ 헬스클럽',
-      timeCreated: '1 day',
+      timeCreated: '1D',
     },
   ]);
 
@@ -94,7 +103,7 @@ const UserPost = () => {
   return (
     // <PostFrame width={1100} height={550}>
     <div className="   p-4 flex flex-col items-center w-[900px] border border-solid border-black ">
-      <div className="flex ">
+      <div className="flex p-6  ">
         <div className=" border border-solid border-black py-2 flex flex-col items-center justify-center w-[200px]  ">
           <img src={profileImg} width={90} alt="profile pic" className="mb-2" />
           <span className="font-[Pretendard]">동훈</span>
@@ -103,7 +112,7 @@ const UserPost = () => {
             <span className="font-[Pretendard]">Lv. 01</span>
           </div>
         </div>
-        <div className="border border-solid border-black  flex flex-col items-center py-2 w-[600px] ">
+        <div className="flex flex-col items-center py-2 w-[600px] ">
           <p className="font-[Pretendard] p-6 font-semibold ">
             솔직히 3대 200도 못들면 헬스 접는게 맞음 ㄹㅇ ㅋㅋ 벤치 40에 스쾃 80
             데드 80만 들어도 3대 ㅋㅋ
@@ -115,11 +124,28 @@ const UserPost = () => {
           />
         </div>
       </div>
-      <div>
+      <p className="font-[Pretendard] m-2 p-2 border-b border-solid border-gray-400 w-full text-right">
+        Total Comments {comments.length}
+      </p>
+      <div className=" w-[800px] ">
+        <div className="   flex flex-col justify-evenly  ">
+          {comments.slice(0, displayComments).map((comment) => (
+            <Comments
+              key={comment.id}
+              profileImg={comment.profileImg}
+              nickname={comment.nickname}
+              label={comment.label}
+              text={comment.text}
+              timeCreated={comment.timeCreated}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="mt-5">
         {comments.length > 3 && (
           <button
             onClick={expandComments}
-            className="font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm"
+            className="  hover:brightness-110 duration-300 cursor-pointer border border-solid bg-green-400 text-white p-2 font-semibold  font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm"
           >
             View More Comments
           </button>
@@ -128,24 +154,11 @@ const UserPost = () => {
         {displayComments > 3 && (
           <button
             onClick={hideComments}
-            className="font-[Pretendard] ml-4 bg-white w-[200px]  rounded-xl text-xm"
+            className="hover:brightness-110 duration-300 cursor-pointer border border-solid  bg-red-400 text-white p-2 font-semibold  font-[Pretendard] ml-4 w-[200px]  rounded-xl text-xm"
           >
             Hide Comments
           </button>
         )}
-      </div>
-      <div className="border border-solid border-black w-[800px] ">
-        <div className="   flex flex-col justify-evenly  ">
-          {comments.slice(0, displayComments).map((comment) => (
-            <Comments
-              key={comment.id}
-              nickname={comment.nickname}
-              label={comment.label}
-              text={comment.text}
-              timeCreated={comment.timeCreated}
-            />
-          ))}
-        </div>
       </div>
     </div>
 
