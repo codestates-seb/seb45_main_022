b68374e16517d2295894ef0e1c50d83982eca59c
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-05 18:59:54 +0900

    💄 design: 이미지 크로퍼 창 UI

--- Client/src/components/title/ImageCropperModal.tsx
+++ Client/src/components/title/ImageCropperModal.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import Cropper, { ReactCropperElement } from 'react-cropper';
 import Backdrop from '../common/Backdrop';
 import Button from '../common/Button';
@@ -7,11 +7,12 @@ interface Props {
   onCloseBtnClick: () => void;
 }
 
+type StepType = 'upload' | 'crop' | 'preview';
+
 const ImageCropperModal = ({ onCloseBtnClick }: Props) => {
   const cropperRef = useRef<ReactCropperElement>(null);
-  // 유저가 첨부한 이미지
+  const [step, setStep] = useState<StepType>('upload');
   const [inputImage, setInputImage] = useState('');
-  // 유저가 선택한 영역만큼 크롭된 이미지
   const [croppedImage, setCroppedImage] = useState('');
 
   const onCrop = () => {
@@ -20,30 +21,88 @@ const ImageCropperModal = ({ onCloseBtnClick }: Props) => {
     setCroppedImage(cropper?.getCroppedCanvas().toDataURL() || '');
   };
 
+  useEffect(() => {
+    if (inputImage !== '') {
+      setStep('crop');
+    }
+  }, [inputImage]);
+
   return (
     <Backdrop>
-      <div className="w-[600px] h-[600px] bg-white p-5 overflow-scroll">
-        <input
-          type="file"
-          accept="image/*"
-          onChange={(e) =>
-            setInputImage(
-              e.target.files ? URL.createObjectURL(e.target.files[0]) : '',
-            )
-          }
-        />
-        <Cropper
-          src={inputImage}
-          crop={onCrop}
-          ref={cropperRef}
-          viewMode={1}
-          aspectRatio={1}
-          style={{ height: 400, width: 400 }}
-        />
-        <img src={croppedImage} />
-        <Button size="medium" onClick={onCloseBtnClick}>
-          Close
-        </Button>
+      <div className="w-screen h-screen flex flex-col justify-center items-center gap-5">
+        <div className="w-[450px] h-[600px] bg-[url('/src/assets/common/modal-frame-cropper.png')] bg-no-repeat bg-cover flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-center items-center gap-10">
+            {step === 'upload' && (
+              <>
+                <div className="w-[260px] h-[260px] bg-[url('/src/assets/common/no-image.png')] bg-no-repeat bg-cover p-[15px] shadow-[0_0_10px_#000]"></div>
+                <form className="w-fit h-fit">
+                  <label className="w-fit h-fit block" htmlFor="image-input">
+                    <Button size="large">Upload Image</Button>
+                  </label>
+                  <input
+                    id="image-input"
+                    className="hidden"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) =>
+                      setInputImage(
+                        e.target.files
+                          ? URL.createObjectURL(e.target.files[0])
+                          : '',
+                      )
+                    }
+                  />
+                </form>
+              </>
+            )}
+            {step === 'crop' && (
+              <>
+                <div className="w-[260px] h-[260px] bg-[url('/src/assets/common/no-image.png')] bg-no-repeat bg-cover p-[15px] shadow-[0_0_10px_#000]">
+                  <Cropper
+                    src={inputImage}
+                    crop={onCrop}
+                    ref={cropperRef}
+                    viewMode={1}
+                    dragMode="move"
+                    aspectRatio={1}
+                    style={{ height: 230, width: 230 }}
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button
+                    onClick={() => {
+                      setInputImage('');
+                      setStep('upload');
+                    }}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    onClick={() => {
+                      setStep('preview');
+                    }}
+                    size="large"
+                  >
+                    Next
+                  </Button>
+                </div>
+              </>
+            )}
+            {step === 'preview' && (
+              <>
+                <img
+                  className="w-[260px] h-[260px] rounded-md"
+                  src={croppedImage}
+                />
+                <div className="flex flex-col gap-3">
+                  <Button onClick={() => setStep('crop')}>Back</Button>
+                  <Button onClick={onCloseBtnClick}>Confirm</Button>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+        <Button onClick={onCloseBtnClick}>Cancel</Button>
       </div>
     </Backdrop>
   );
