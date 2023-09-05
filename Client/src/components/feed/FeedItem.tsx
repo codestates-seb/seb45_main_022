b73d2bdf9868403fac3d9612e9b55262268f46be
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-05 21:54:31 +0900

    ✨ feat: FeedItem 컴포넌트 구현

--- Client/src/components/feed/FeedItem.tsx
+++ Client/src/components/feed/FeedItem.tsx
@@ -1,32 +1,28 @@
 import feedItem from '../../assets/feed/feedItem.png';
+import likeButton from '../../assets/feed/likeButton.png';
+import commentButton from '../../assets/feed/commentButton.png';
+import { STATUS_ICON } from '../../utility/status';
+import { Feed } from '../../api/feed';
+import { StatusCode } from '../../api/category';
 
 interface FeedItemProps {
-  image: string;
-  nickname: string;
-  icon: string;
-  level: number;
-  uploadImage: string;
-  content: string;
-  timestamp: string;
-  likeIcon: string;
-  likeCount: number;
-  commentIcon: string;
-  commentCount: number;
+  feed: Feed;
 }
 
-const FeedItem: React.FC<FeedItemProps> = ({
-  image,
-  nickname,
-  icon,
-  level,
-  uploadImage,
-  content,
-  timestamp,
-  likeIcon,
-  likeCount,
-  commentIcon,
-  commentCount,
-}) => {
+const FeedItem = ({ feed }: FeedItemProps) => {
+  const {
+    nickName,
+    profileImage,
+    level,
+    statId,
+    body,
+    likeCount,
+    commentCount,
+    createdAt,
+  } = feed;
+
+  const typeStatId: StatusCode = statId;
+
   const FeedBoard = {
     backgroundImage: `url(${feedItem})`,
     backgroundSize: 'cover',
@@ -37,53 +33,52 @@ const FeedItem: React.FC<FeedItemProps> = ({
     <div className="w-[295px] h-[137px]" style={FeedBoard}>
       <div className="w-full h-full p-7 flex">
         {/* 왼쪽 구간 (전체 너비 1/3) */}
-        <div className="w-[70px] h-full flex flex-col justify-between items-start gap-1">
+        <div className="w-[70px] h-full flex flex-col justify-between items-start gap-1 cursor-pointer">
           <div className="w-[35px] h-[35px] mt-[3px] ml-[10px]">
-            <img src={image} alt="profile" />
+            <img src={profileImage} alt="profile" />
           </div>
           <div
             className="w-[45px] mt-1 ml-[6px] flex justify-center items-center"
             style={{
               fontFamily: 'Pretendard',
-              fontSize: nickname.length > 3 ? '0.5rem' : '1rem',
+              fontSize: nickName.length > 3 ? '0.5rem' : '1rem',
             }}
           >
-            {nickname}
+            {nickName}
           </div>
-          <div className="w-full flex justify-start items-center gap-1 -mt-1">
-            <div className=" w-[10px] ">
-              <img src={icon} alt="아이콘" />
+          <div className="w-full flex justify-start items-center -mt-1">
+            <div className="w-[10px]">
+              <img src={STATUS_ICON[typeStatId]} alt="스탯 아이콘" />
             </div>
-            <div className="text-[0.1rem]">Lv {level}</div>
+            <div className="text-[10px]">Lv_{level}</div>
           </div>
         </div>
 
         {/* 오른쪽 구간 (전체 너비 2/3) */}
-        {/* <div className="w-[66%] ">
-          이미지
-          <img src={uploadImage} alt="업로드 이미지" />
-          
-          이미지 위에 텍스트
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-end p-4">
-            내용
-            <div>{content}</div>
-            
-            생성일시
-            <div>{timestamp}</div>
-            
-            좋아요 아이콘 및 개수
-            <div>
-              <img src={likeIcon} alt="좋아요 아이콘" />
+        <div className="w-full h-full flex flex-col justify-between items-end">
+          <div className="w-full h-[35px] font-[Pretendard] cursor-pointer">
+            {body.length > 20 ? `${body.slice(0, 20)}...` : body}
+          </div>
+          <div className="w-full flex justify-end items-center gap-1.5">
+            <div className="text-[10px] font-[Pretendard]">
+              {new Date(createdAt).toLocaleTimeString('ko-KR', {
+                year: '2-digit',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </div>
+            <div className="flex gap-1 text-[10px] cursor-pointer">
+              <img src={likeButton} alt="좋아요 아이콘" width={15} />
               {likeCount}
             </div>
-            
-            코멘트 아이콘 및 개수
-            <div>
-              <img src={commentIcon} alt="코멘트 아이콘" />
+            <div className="flex gap-1 text-[10px] mr-1 cursor-pointer">
+              <img src={commentButton} alt="코멘트 아이콘" width={15} />
               {commentCount}
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
