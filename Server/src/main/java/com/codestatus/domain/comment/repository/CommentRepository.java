3d4d7cf00c2d763d907d6a0d75e64fbe8b8c5987
Island-Fever <handoseok@gmail.com>
2023-09-05 21:34:29 +0900

    🐛 fix: 유저 탈퇴시 작성한 feed, comment 의 deleted 상태를 true로 변경

--- Server/src/main/java/com/codestatus/domain/comment/repository/CommentRepository.java
+++ Server/src/main/java/com/codestatus/domain/comment/repository/CommentRepository.java
@@ -4,9 +4,14 @@ import com.codestatus.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 //    @Query(nativeQuery = true, value = "SELECT * FROM comment as c WHERE c.comment_id=:commentId AND c.deleted=:deleted")
     Optional<Comment> findCommentByCommentIdAndDeleted(long commentId, boolean deleted);
+
+    List<Comment> findAllByUser_UserIdAndDeletedIsFalse(long userId);
+
+    List<Comment> findAllByFeed_FeedIdAndDeletedIsFalse(long feedId);
 }
