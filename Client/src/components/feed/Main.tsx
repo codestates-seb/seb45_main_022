7ad5e7bdf19fbbed67ec3cf282155f5eaa723be0
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-06 20:54:56 +0900

    ✨ feat: 피드 작성 라우트 이동 버튼 추가

--- Client/src/components/feed/Main.tsx
+++ Client/src/components/feed/Main.tsx
@@ -3,6 +3,7 @@ import SearchBar from './SearchBar';
 import FeedItem from './FeedItem';
 import { Feed } from '../../api/feed';
 import { CategoryCode } from '../../api/category';
+import { Link } from 'react-router-dom';
 
 interface Props {
   feedList: Feed[];
@@ -11,7 +12,7 @@ interface Props {
 
 const Main = ({ feedList, categoryCode }: Props) => {
   return (
-    <div className="w-full h-[31.25rem] flex flex-col justify-start items-center mt-[55px] ml-1">
+    <div className="relative w-full h-[31.25rem] flex flex-col justify-start items-center mt-[55px] ml-1">
       <div className="w-full h-[3rem] flex justify-around items-center bg-[#f8d8ae] gap-[20rem]">
         <FilterButton />
         <SearchBar />
@@ -25,6 +26,9 @@ const Main = ({ feedList, categoryCode }: Props) => {
           <div>Loading...</div>
         )}
       </div>
+      <Link to="post">
+        <button className="absolute bottom-5 right-5 w-[50px] h-[50px] bg-[#f8d8ae] bg-[url('/src/assets/icons/btn-pencil.png')] bg-no-repeat bg-cover rounded-full p-2 duration-300 shadow-[0_0_5px_#e1772d] hover:brightness-110 hover:scale-110" />
+      </Link>
     </div>
   );
 };
