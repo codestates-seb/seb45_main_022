47195b1eb4aac4bd383d6c9618b3f36edfeb5496
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-08 09:21:38 +0900

    ♻️ refactor: 서버에서 최신순/주간 베스트 API 따로 만들어줘서 클라이언트쪽에 있는 기능 삭제

--- Client/src/components/feed/FilterButton.tsx
+++ Client/src/components/feed/FilterButton.tsx
@@ -1,11 +1,6 @@
 import { useState } from 'react';
 
-const FilterButton = ({
-  handleFilterNewest,
-  handleFilterByBest,
-  sortByNewest,
-  sortByBest,
-}) => {
+const FilterButton = () => {
   const buttonStyle = {
     fontFamily: 'Pretendard',
     padding: '0.5rem 1rem',
@@ -25,16 +20,10 @@ const FilterButton = ({
 
   return (
     <div className="flex justify-center items-center gap-5">
-      <button
-        style={sortByNewest ? clickedButtonStyle : unclickedButtonStyle}
-        onClick={handleFilterNewest}
-      >
+      <button style={sortByNewest ? clickedButtonStyle : unclickedButtonStyle}>
         최신순
       </button>
-      <button
-        style={sortByBest ? clickedButtonStyle : unclickedButtonStyle}
-        onClick={handleFilterByBest}
-      >
+      <button style={sortByBest ? clickedButtonStyle : unclickedButtonStyle}>
         주간 베스트
       </button>
     </div>
