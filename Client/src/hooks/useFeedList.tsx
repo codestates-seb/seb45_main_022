9388c3fa33e225e511539410b410d9aa529e08e9
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-09 19:57:14 +0900

    ♻️ refactor: cleanup

--- Client/src/hooks/useFeedList.tsx
+++ Client/src/hooks/useFeedList.tsx
@@ -2,13 +2,8 @@ import { useQuery } from 'react-query';
 import { getFeedList, getWeeklyBest } from '../api/feed';
 import { CategoryCode } from '../api/category';
 import { getFilterdFeedList } from '../api/feed';
-import { FeedId } from '../api/feed';
 
-function useFeedList(
-  categoryCode: CategoryCode,
-  feedId: FeedId,
-  keyword?: string,
-) {
+function useFeedList(categoryCode: CategoryCode, keyword?: string) {
   const feedListQuery = useQuery(
     ['feedList', categoryCode],
     () => getFeedList(categoryCode),
@@ -27,7 +22,7 @@ function useFeedList(
     {
       staleTime: 1000 * 60,
       cacheTime: 1000 * 60 * 10,
-      enabled: !!feedId,
+      enabled: !!categoryCode,
       refetchOnWindowFocus: false,
       retry: 2,
     },
