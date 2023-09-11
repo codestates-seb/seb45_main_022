b3eb0f61324d5105e24d72ae188e63a385a61aa9
jjo3ys <jjo3ys@gmail.com>
2023-09-11 14:31:06 +0900

    fix: add NoArgsConstructor in Comment post Dto

--- Server/src/main/java/com/codestatus/domain/comment/dto/CommentPostDto.java
+++ Server/src/main/java/com/codestatus/domain/comment/dto/CommentPostDto.java
@@ -2,13 +2,14 @@ package com.codestatus.domain.comment.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentPostDto {
-    private Long id;
     @NotBlank
     private String body;
 }
