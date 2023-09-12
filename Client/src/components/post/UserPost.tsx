3dca22d2ef74e8f1ac9faf3c46ed1e12daf8d783
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-12 11:15:14 +0900

    💄 design: 상세 게시글 전체적인 UI 변경

--- Client/src/components/post/UserPost.tsx
+++ Client/src/components/post/UserPost.tsx
@@ -1,11 +1,12 @@
 import icon from '../../assets/icons/status-strength.png';
-import { FaThumbsUp } from 'react-icons/fa';
+import { FaThumbsUp, FaCommentDots } from 'react-icons/fa';
 
 import useUserFeed from '../../hooks/useUserFeed';
-// import { STATUS_ICON } from '../../utility/status';
-// import { CATEGORY_STATUS_MAP } from '../../utility/category';
+import { STATUS_ICON } from '../../utility/status';
+import { CATEGORY_STATUS_MAP } from '../../utility/category';
 import { useState } from 'react';
 import { Feed } from '../../api/feed';
+import Comments from './Comments';
 // import { Feed } from '../../api/feed';
 
 interface PostProps {
@@ -22,7 +23,7 @@ interface Comment {
   createdDate: string;
 }
 
-const UserPost = ({ setOpenFeedItem, feed }: PostProps) => {
+const UserPost = ({ setOpenFeedItem, feed, categoryCode }: PostProps) => {
   // const [likes, setlikes] = useState(3);
 
   // const handleLikePost = () => {
@@ -64,7 +65,7 @@ const UserPost = ({ setOpenFeedItem, feed }: PostProps) => {
   };
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return null;
   }
 
   if (isError) {
@@ -78,16 +79,18 @@ const UserPost = ({ setOpenFeedItem, feed }: PostProps) => {
     >
       <div
         onClick={handleContainerClick}
-        className=" bg-white relative top-[40px] right-0 bottom-[40px] left-0 mx-auto p-[16px] flex flex-col items-center w-[800px] rounded-[12px] border-[20px] border-solid border-[#444657]"
+        // className=" bg-white relative top-[40px] right-0 bottom-[40px] left-0 mx-auto p-[16px] flex flex-col items-center w-[800px] rounded-[12px] border-[20px] border-solid border-[#444657]"
+        className="  bg-white relative top-[50px] right-0 bottom-[40px] left-0 mx-auto  flex justify-evenly  w-[1000px] h-[80vh] rounded-[12px] "
       >
         <button
           onClick={handleCloseScreen}
-          className="absolute top-[16px] right-[16px] text-3xl "
+          className="absolute top-[6px] right-[8px] text-3xl "
         >
           X
         </button>
-        <div className="flex justify-between py-[16px]">
-          <div className="px-[16px] flex flex-col items-center border-[1px] border-solid border-gray-400 ">
+        {/* <div className=" justify-between py-[16px] flex flex-col bg-orange-200"> */}
+        <div className=" justify-between  flex flex-col bg-orange-200">
+          <div className=" flex flex-col items-center  p-4 ">
             <img
               src={userFeed.profileImage}
               width={90}
@@ -97,12 +100,12 @@ const UserPost = ({ setOpenFeedItem, feed }: PostProps) => {
             <span className="font-[Pretendard] font-semibold">
               {userFeed.nickname}
             </span>
-            <div className="flex mt-1 items-center justify-around w-[100%]">
+            <div className="flex mt-1 items-center justify-center w-[100%]">
               <img
-                // src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
-                alt="스탯 아이콘"
+                src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
+                alt="stat icon"
               />
-              <span className="font-[Pretendard]">Lv. {userFeed.level}</span>
+              <span className="">Lv. {userFeed.level}</span>
             </div>
           </div>
           <div className="flex flex-col items-center justify-between  py-2 w-[500px]">
@@ -111,96 +114,103 @@ const UserPost = ({ setOpenFeedItem, feed }: PostProps) => {
               className=" font-[Pretendard] p-6 font-semibold overflow-hidden overflow-ellipsis"
             />
           </div>
-        </div>
-        <div className="flex items-center justify-between p-[16px] border-[1px] border-solid border-gray-400 w-full">
-          <div>
-            <span className=" text-sm font-semibold  rounded-xl p-3">
-              Total Comments
-            </span>
-            <span className="ml-2 text-m text-gray-500 font-semibold">
-              {userFeed.comments.length}
-            </span>
-          </div>
-          <div className="relative mr-6 ">
-            <button
-              // onClick={handleLikePost}
-              className="hover:brightness-110 duration-200 hover:text-green-400 rounded-xl text-xl font-semibold "
-            >
-              <FaThumbsUp />
-            </button>
-            <span className="absolute  top-0 ml-3 text-sm text-gray-500 font-semibold ">
-              {userFeed.likeCount}
-            </span>
-          </div>
-        </div>
-        <div className="flex items-center justify-center border-[1px] border-solid border-gray-400 w-full py-[16px]">
-          <input
-            type="search"
-            // onChange={(e) => setAddComment(e.target.value)}
-            // value={addComment}
-            className="border border-solid border-gray-400 rounded-xl p-2 font-[Pretendard] w-[400px]"
-          />
-          <button className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-sky-500 text-white py-[8px] px-[12px] text-sm font-semibold ml-[16px] w-[200px] rounded-[12px]">
-            Add a comment
-          </button>
-        </div>
-
-        <div className="max-w-[800px] w-full">
-          {userFeed.comments
-            .slice(0, displayComments)
-            .map((comment: Comment) => (
-              <div
-                key={comment.commentId}
-                className="border-b border-solid border-gray-400  py-2 flex p-4 "
+          {/* <div className="flex items-center justify-between p-[16px] border-[1px] border-solid border-gray-400 w-full"> */}
+          <div className="flex items-center justify-end p-[8px] border-[1px] border-solid border-gray-400 w-full">
+            <div className="mr-4">
+              <button
+                // onClick={handleLikePost}
+                className="hover:brightness-110 duration-200 hover:text-green-400 rounded-xl text-xl font-semibold "
               >
-                <div className="flex flex-col items-center justify-center w-20">
-                  <img
-                    src={comment.profileImage}
-                    alt="profile image"
-                    width={45}
-                  />
-                  <span className="font-[Pretendard] font-semibold">
-                    {comment.nickname}
-                  </span>
+                <FaThumbsUp />
+              </button>
+              <span className="  top-0 ml-3 text-sm text-gray-500 font-semibold ">
+                {userFeed.likeCount}
+              </span>
+            </div>
+            <div className="relative mr-2 ">
+              <button className="hover:brightness-110 duration-200 hover:text-green-400 rounded-xl text-xl font-semibold ">
+                <FaCommentDots />
+              </button>
+              <span className="ml-2 text-m text-gray-500 font-semibold">
+                {userFeed.comments.length}
+              </span>
+            </div>
+          </div>
+        </div>
 
-                  <div className="flex mt-1 items-center justify-around w-[100%]">
-                    <img src={icon} alt="muscle icon" width={15} />
-                    <span className="font-[Pretendard] text-sm">
-                      Lv. {comment.level}
+        <div className="flex flex-col w-full mt-[1rem]">
+          <div className="flex items-center justify-between "></div>
+
+          {/* <div className="max-w-[800px] w-full"> */}
+          <div className="h-screen overflow-auto scrollbar-width-none">
+            {userFeed.comments
+              .slice(0, displayComments)
+              .map((comment: Comment) => (
+                <div key={comment.commentId} className="  p-4  my-2 flex  ">
+                  {/* <div className="flex flex-col items-center justify-center w-20"> */}
+                  <div className="flex flex-col items-center justify-center  w-[8rem] ">
+                    <img
+                      src={comment.profileImage}
+                      alt="profile image"
+                      width={45}
+                    />
+                    <span className="font-[Pretendard] font-semibold">
+                      {comment.nickname}
+                    </span>
+
+                    {/* <div className="flex mt-1 items-center justify-around w-[100%]"> */}
+                    <div className="flex mt-1 items-center justify-between ">
+                      <img src={icon} alt="muscle icon" width={16} />
+                      <span className="font-[Pretendard] text-sm ml-[0.5rem]">
+                        Lv. {comment.level}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex text-sm   w-full p-4">
+                    <span className="font-[Pretendard] font-normal">
+                      {comment.body}
+                    </span>
+                  </div>
+                  <div className="w-10 text-center">
+                    <span className="font-[Pretendard] text-sm text-gray-500 ">
+                      {comment.createdDate}
                     </span>
                   </div>
                 </div>
-                <div className="flex text-sm   w-full p-4">
-                  <span className="font-[Pretendard] font-normal">
-                    {comment.body}
-                  </span>
-                </div>
-                <div className="w-10 text-center">
-                  <span className="font-[Pretendard] text-sm text-gray-500 ">
-                    {comment.createdDate}
-                  </span>
-                </div>
-              </div>
-            ))}
-        </div>
-        <div className="mt-5">
-          {userFeed.comments.length > 3 && (
-            <button
-              onClick={expandComments}
-              className=" hover:brightness-110 duration-300 cursor-pointer border border-solid bg-emerald-500 text-white p-[12px] font-semibold text-sm ml-[16px] rounded-[12px]"
-            >
-              View More Comments
-            </button>
-          )}
+              ))}
+          </div>
+          {/* <div className="">
+            {userFeed.comments.length > 3 && (
+              <button
+                onClick={expandComments}
+                className=" hover:brightness-110 duration-300 cursor-pointer border border-solid bg-emerald-500 text-white p-[12px] font-semibold text-sm ml-[16px] rounded-[12px]"
+              >
+                View More Comments
+              </button>
+            )}
 
-          {displayComments > 3 && (
-            <button
-              onClick={hideComments}
-              className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-red-400 text-white p-[12px] font-semibold text-sm ml-[16px] rounded-[12px]"
-            >
-              Hide Comments
+            {displayComments > 3 && (
+              <button
+                onClick={hideComments}
+                className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-red-400 text-white p-[12px] font-semibold text-sm ml-[16px] rounded-[12px]"
+              >
+                Hide Comments
+              </button>
+            )}
+          </div> */}
+
+          <div className="flex items-center justify-center mx-auto p-4">
+            <input
+              type="text"
+              placeholder="댓글"
+              // onChange={(e) => setAddComment(e.target.value)}
+              // value={addComment}
+              className="border border-solid border-gray-400 rounded-xl p-2 font-[Pretendard] w-[20rem]"
+            />
+            <button className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-sky-500 text-white h-[2.5rem] w-[2.5rem] ml-[1rem] text-sm font-semibold rounded-[12px]">
+              +
             </button>
-          )}
+          </div>
         </div>
       </div>
     </div>
