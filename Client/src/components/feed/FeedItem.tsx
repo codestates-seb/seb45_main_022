bb50a93a0d852b5b8ab4c694eda701979e3c6892
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-14 04:47:03 +0900

    💄 design: 피드 아이템 스타일 수정

--- Client/src/components/feed/FeedItem.tsx
+++ Client/src/components/feed/FeedItem.tsx
@@ -23,35 +23,35 @@ const LatestFeedItem = ({ feed, categoryCode }: FeedItemProps) => {
 
   return (
     <>
-      <div className="w-[295px] h-[137px] hover:cursor-pointer m-[12px] bg-cover bg-center bg-feedBox">
-        <div className="w-full h-full p-[28px] flex">
+      <div className="w-[300px] h-[140px] m-[12px] bg-cover bg-center bg-feedBox">
+        <div className="w-full h-full p-[28px] flex flex-row justify-between">
           {/* 왼쪽 구간 (전체 너비 1/3) */}
-          <div className="w-[70px] h-full flex flex-col justify-between items-start gap-[4px] cursor-pointer">
-            <div className="w-[35px] h-[35px] mt-[3px] ml-[10px]">
+          <div className="w-[60px] h-full flex flex-col justify-between items-center">
+            <div className="bg-[url('/src/assets/common/profile-frame.png')] bg-cover bg-no-repeat w-[50px] h-[50px] p-[5px]">
               <img src={profileImage} alt="profile" />
             </div>
             <div
-              className={`w-full flex justify-center ${
+              className={`w-full flex justify-center items-center ${
                 nickname.length > 3 ? 'text-xs' : 'text-sm'
               }`}
             >
               {nickname}
             </div>
-            <div className="w-full flex justify-start items-center -mt-[4px] gap-[2px]">
+            <div className="w-full flex justify-center items-center -mt-[4px] gap-[2px]">
               <div className="w-[10px] -mt-[3px]">
                 <img
                   src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
                   alt="스탯 아이콘"
                 />
               </div>
-              <div className="text-[0.625rem]">Lv_{level}</div>
+              <div className="text-[0.625rem]">Lv.{level}</div>
             </div>
           </div>
 
           {/* 오른쪽 구간 (전체 너비 2/3) */}
-          <div className="w-full h-full flex flex-col justify-between items-end">
-            <div className="w-full h-[35px] -mr-[7px] -mt-[2px] font-[Pretendard] cursor-pointer">
-              {body.length > 23 ? `${body.slice(0, 23)}...` : body}
+          <div className="w-[175px] h-full flex flex-col justify-between items-end">
+            <div className="w-full h-[50px] pr-[5px] font-[Pretendard] overflow-hidden text-ellipsis whitespace-nowrap">
+              {body}
             </div>
             <div className="w-full flex justify-end items-center gap-[6px]">
               <div className="text-[0.625rem] font-[Pretendard]">
@@ -63,11 +63,11 @@ const LatestFeedItem = ({ feed, categoryCode }: FeedItemProps) => {
                   minute: '2-digit',
                 })}
               </div>
-              <div className="flex gap-[4px] text-[0.625rem] cursor-pointer">
+              <div className="flex gap-[4px] text-[0.625rem]">
                 <img src={likeButton} alt="좋아요 아이콘" width={15} />
                 {likeCount}
               </div>
-              <div className="flex gap-[4px] text-[0.625rem] mr-[4px] cursor-pointer">
+              <div className="flex gap-[4px] text-[0.625rem] mr-[4px]">
                 <img src={commentButton} alt="코멘트 아이콘" width={15} />
                 {commentCount}
               </div>
