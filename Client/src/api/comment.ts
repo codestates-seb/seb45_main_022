927eb375f24796469beb472f6b7f52479e1c7594
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-13 13:29:44 +0900

    ✨ feat: 댓글 수정 및 삭제 axios 추가

--- Client/src/api/comment.ts
+++ Client/src/api/comment.ts
@@ -5,10 +5,30 @@ interface UseCommentProps {
     body: string;
 }
 
+interface useEditComment {
+    commentId: number;
+    body: string;
+}
+
+
+
 export const addCommentData = async ({ feedId, body }: UseCommentProps) => {
     const response = await customAxios.post(`/comment/${feedId}`, {
         body
     });
     return response;
 
+};
+
+
+export const editCommentData = async ({ commentId, body }: useEditComment) => {
+    const response = await customAxios.put(`/comment/${commentId}`, {
+        body
+    });
+    return response;
+};
+
+export const deleteCommentData = async ({ commentId }: useEditComment) => {
+    const response = await customAxios.delete(`/comment/${commentId}`);
+    return response;
 };
