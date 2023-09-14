1471b2665d994e12f0bc04355d2140a70f3b1bb9
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-14 17:44:21 +0900

    ✨ feat: Header에 로그인한 개인 유저 정보를 연동하는 기능 추가

--- Client/src/components/feed/Header.tsx
+++ Client/src/components/feed/Header.tsx
@@ -6,6 +6,9 @@ import {
 } from '../../utility/category';
 import { useNavigate } from 'react-router-dom';
 import { CategoryCode } from '../../api/category';
+import useUserInfoQuery from '../../hooks/useUserInfoQuery';
+import Backdrop from '../common/Backdrop';
+import LoadingBar from '../common/LoadingBar';
 
 interface Props {
   categoryCode: CategoryCode;
@@ -14,67 +17,96 @@ interface Props {
 const Header = ({ categoryCode }: Props) => {
   const nav = useNavigate();
 
-  return (
-    <div
-      className={`w-full h-[60px] flex justify-center items-center gap-[220px]`}
-    >
-      {/* 닉네임 */}
-      <p
-        className="w-[144px] text-[1.5rem] font-bold mt-[100px] cursor-pointer"
-        style={{ fontFamily: 'Pretendard' }}
-      >
-        ⚔️ 헬창용사님
-      </p>
+  const { isLoading, data: userInfo } = useUserInfoQuery();
 
-      {/* 아이콘, 카테고리 이름 */}
+  if (isLoading)
+    return (
+      <Backdrop>
+        <LoadingBar />
+      </Backdrop>
+    );
+
+  if (userInfo) {
+    return (
       <div
-        className="w-[250px] flex justify-between items-center cursor-pointer ml-[8px]"
-        onClick={() => {
-          nav(`/map/${CATEGORY_STATUS_MAP[categoryCode]}`);
-        }}
+        className={`w-full h-[60px] flex justify-center items-center gap-[220px]`}
       >
-        <img
-          className="w-[50px] h-[50px]"
-          src={CATEGORY_ICON[categoryCode]}
-          alt="Icon"
-        />
-        <span className="text-[2rem] font-bold mb-[8px]">
-          {CATEGORY_NAME[categoryCode]}
-        </span>
-        <img
-          className="w-[50px] h-[50px]"
-          src={CATEGORY_ICON[categoryCode]}
-          alt="Icon"
-        />
-      </div>
+        {/* 닉네임 */}
+        <p
+          className="w-[144px] text-[1.5rem] font-bold mt-[100px] cursor-pointer"
+          style={{ fontFamily: 'Pretendard' }}
+        >
+          ⚔️ {userInfo.nickname}_님
+        </p>
 
-      {/* 스탯 아이콘, 레벨, 경험치 바 */}
-      <div className="flex flex-col justify-center items-end gap-[8px] cursor-pointer mt-[100px]">
-        <div className="flex gap-[16px]">
+        {/* 아이콘, 카테고리 이름 */}
+        <div
+          className="w-[250px] flex justify-between items-center cursor-pointer ml-[8px]"
+          onClick={() => {
+            nav(`/map/${CATEGORY_STATUS_MAP[categoryCode]}`);
+          }}
+        >
           <img
-            className="w-[16px] h-[16px]"
-            src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
-            alt="titleIcon"
+            className="w-[50px] h-[50px]"
+            src={CATEGORY_ICON[categoryCode]}
+            alt="Icon"
           />
-          <span className="text-[1rem]">Lv.100</span>
+          <span className="text-[2rem] font-bold mb-[8px]">
+            {CATEGORY_NAME[categoryCode]}
+          </span>
+          <img
+            className="w-[50px] h-[50px]"
+            src={CATEGORY_ICON[categoryCode]}
+            alt="Icon"
+          />
         </div>
 
-        <div className="flex">
-          <div className="relative w-[144px] h-[16px] p-[8px] bg-gray-300 rounded-full overflow-hidden flex justify-start items-center">
-            <div
-              className="h-full bg-yellow-500 rounded-full"
-              style={{
-                width: `${(0 / 100) * 100}%`,
-              }}
-            ></div>
-            <span className="absolute left-[16px] text-xs text-gray-400">
-              {`${0} / ${100}`}
+        {/* 스탯 아이콘, 레벨, 경험치 바 */}
+        <div className="flex flex-col justify-center items-end gap-[8px] cursor-pointer mt-[100px]">
+          <div className="flex gap-[16px]">
+            <img
+              className="w-[16px] h-[16px]"
+              src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
+              alt="titleIcon"
+            />
+            <span className="text-[1rem]">
+              Lv.
+              {
+                userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                  .statLevel
+              }
             </span>
+          </div>
+
+          <div className="flex">
+            <div className="relative w-[144px] h-[16px] p-[8px] bg-gray-300 rounded-full overflow-hidden flex justify-start items-center">
+              <div
+                className="absolute h-full left-[0px] bg-yellow-500 rounded-full"
+                style={{
+                  width: `${
+                    (userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                      .statExp /
+                      userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                        .requiredExp) *
+                    100
+                  }%`,
+                }}
+              ></div>
+              <span className="absolute left-[16px] text-xs text-gray-400">
+                {`${
+                  userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                    .statExp
+                } / ${
+                  userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                    .requiredExp
+                }`}
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Header;
