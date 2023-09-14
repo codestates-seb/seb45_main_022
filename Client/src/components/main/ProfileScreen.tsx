f6f9ba3452e74dd3a3eec2144c6d29d6487377d8
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-14 16:09:21 +0900

    ✨feat : 나의 피드로 바로가기 path 추가

--- Client/src/components/main/ProfileScreen.tsx
+++ Client/src/components/main/ProfileScreen.tsx
@@ -13,6 +13,7 @@ interface UserFeed {
   body: string;
   commentCount: number;
   createdAt: string;
+  statId: number;
 }
 
 const ProfileScreen = () => {
@@ -84,20 +85,22 @@ const ProfileScreen = () => {
               {tab === 'post' && (
                 <div className="w-full h-full overflow-y-scroll">
                   {userFeedList.map((feed: UserFeed) => (
-                    <div
-                      className="cursor-pointer font-[Pretendard]  flex flex-col justify-between  text-[10px] w-[90%] h-[3.25rem]  rounded-lg p-2 mx-auto mb-4 border border-black  bg-[#ffc98f]"
-                      key={feed.feedId}
-                    >
-                      <p className="text-[12px] font-bold">
-                        {feed.body.length > 25
-                          ? feed.body.slice(0, 25) + '...'
-                          : feed.body}
-                      </p>
-                      <div className="flex items-center justify-between">
-                        <span>{feed.commentCount} comments</span>
-                        <span>{feed.createdAt.slice(0, 10)}</span>
+                    <Link to={`/feed/${feed.statId}/detail/${feed.feedId}`}>
+                      <div
+                        className="cursor-pointer font-[Pretendard]  flex flex-col justify-between  text-[10px] w-[90%] h-[3.25rem]  rounded-lg p-2 mx-auto mb-4 border border-black  bg-[#ffc98f]"
+                        key={feed.feedId}
+                      >
+                        <p className="text-[12px] font-bold">
+                          {feed.body.length > 25
+                            ? feed.body.slice(0, 25) + '...'
+                            : feed.body}
+                        </p>
+                        <div className="flex items-center justify-between">
+                          <span>{feed.commentCount} comments</span>
+                          <span>{feed.createdAt.slice(0, 10)}</span>
+                        </div>
                       </div>
-                    </div>
+                    </Link>
                   ))}
                 </div>
               )}
