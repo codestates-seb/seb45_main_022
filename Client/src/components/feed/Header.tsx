8c602bc692dc15b9e440a1b2f65a8b1ddbf85bf1
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-15 10:39:49 +0900

    🐛 fix: Header 모달창 클릭 시, 정상적으로 Route 이동 해결

--- Client/src/components/feed/Header.tsx
+++ Client/src/components/feed/Header.tsx
@@ -12,8 +12,6 @@ import Backdrop from '../common/Backdrop';
 import LoadingBar from '../common/LoadingBar';
 import { BackButton } from '../../components/common/BackButton';
 import { FrontButton } from '../../components/common/BackButton';
-import ProfileScreen from '../main/ProfileScreen';
-import StatusScreen from '../main/StatusScreen';
 
 interface Props {
   categoryCode: CategoryCode;
@@ -21,18 +19,8 @@ interface Props {
 
 const Header = ({ categoryCode }: Props) => {
   const nav = useNavigate();
-  const [isProfileModalOpen, setProfileModalOpen] = useState<boolean>(false);
-  const [isStatModalOpen, setIsStatModalOpen] = useState<boolean>(false);
 
   const { isLoading, data: userInfo } = useUserInfoQuery();
-
-  const handleNicknameClick = () => {
-    setProfileModalOpen((prev) => !prev);
-  };
-
-  const handleLevelClick = () => {
-    setIsStatModalOpen((prev) => !prev);
-  };
 
   if (isLoading)
     return (
@@ -47,15 +35,10 @@ const Header = ({ categoryCode }: Props) => {
         {/* 닉네임 */}
 
         <Link to={`/feed/${categoryCode}/profile`}>
-          <p
-            className="w-[144px] text-[1.5rem] font-bold mt-[100px] cursor-pointer font-[Pretendard]"
-            onClick={handleNicknameClick}
-          >
+          <p className="w-[144px] text-[1.5rem] font-bold mt-[100px] cursor-pointer font-[Pretendard]">
             ⚔️ {userInfo.nickname}_님
           </p>
         </Link>
-        {/* 프로필 모달 표시 */}
-        {isProfileModalOpen && <ProfileScreen />}
 
         {/* 아이콘, 카테고리 이름 */}
         <div className="mt-[60px] w-[35px] ml-3">
@@ -87,57 +70,56 @@ const Header = ({ categoryCode }: Props) => {
           )}
         </div>
         {/* 스탯 아이콘, 레벨, 경험치 바 */}
-        <div
-          className="flex flex-col justify-center items-end gap-[8px] cursor-pointer mt-[100px]"
-          onClick={handleLevelClick}
-        >
-          <div className="flex gap-[16px]">
-            <img
-              className="w-[16px] h-[16px]"
-              src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
-              alt="titleIcon"
-            />
-            <span className="text-[1rem]">
-              Lv.
-              {
-                userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                  .statLevel
-              }
-            </span>
-          </div>
-
-          <div className="flex">
-            <div className="relative w-[144px] h-[16px] p-[8px] bg-gray-300 rounded-full overflow-hidden flex justify-start items-center">
-              <div
-                className="absolute h-full left-[0px] bg-yellow-500 rounded-full"
-                style={{
-                  width: `${
-                    (userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                      .statExp /
-                      (userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                        .requiredExp +
-                        userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                          .statExp)) *
-                    100
-                  }%`,
-                }}
-              ></div>
-              <span className="absolute left-[16px] text-xs text-gray-400">
-                {`${
+        <Link to={`/feed/${categoryCode}/status`}>
+          <div className="flex flex-col justify-center items-end gap-[8px] cursor-pointer mt-[100px]">
+            <div className="flex gap-[16px]">
+              <img
+                className="w-[16px] h-[16px]"
+                src={STATUS_ICON[CATEGORY_STATUS_MAP[categoryCode]]}
+                alt="titleIcon"
+              />
+              <span className="text-[1rem]">
+                Lv.
+                {
                   userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                    .statExp
-                } / ${
-                  userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                    .requiredExp +
-                  userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
-                    .statExp
-                }`}
+                    .statLevel
+                }
               </span>
             </div>
+
+            <div className="flex">
+              <div className="relative w-[144px] h-[16px] p-[8px] bg-gray-300 rounded-full overflow-hidden flex justify-start items-center">
+                <div
+                  className="absolute h-full left-[0px] bg-yellow-500 rounded-full"
+                  style={{
+                    width: `${
+                      (userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                        .statExp /
+                        (userInfo.statuses[
+                          CATEGORY_STATUS_MAP[categoryCode] - 1
+                        ].requiredExp +
+                          userInfo.statuses[
+                            CATEGORY_STATUS_MAP[categoryCode] - 1
+                          ].statExp)) *
+                      100
+                    }%`,
+                  }}
+                ></div>
+                <span className="absolute left-[16px] text-xs text-gray-400">
+                  {`${
+                    userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                      .statExp
+                  } / ${
+                    userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                      .requiredExp +
+                    userInfo.statuses[CATEGORY_STATUS_MAP[categoryCode] - 1]
+                      .statExp
+                  }`}
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
-        {/* 레벨 모달 표시 */}
-        {isStatModalOpen && <StatusScreen />}
+        </Link>
       </div>
     );
   }
