b34ba9f6000d5115dad42903b107c543cee4ec5e
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-17 23:42:39 +0900

    ✨ feat: 코멘트 무한 스크롤 적용

--- Client/src/components/feed-detail/CommentSection.tsx
+++ Client/src/components/feed-detail/CommentSection.tsx
@@ -5,6 +5,7 @@ import { FormEventHandler, useRef } from 'react';
 import usePostCommentMutation from '../../hooks/usePostCommentMutation';
 import useCommentListQuery from '../../hooks/useCommentListQuery';
 import LoadingBar from '../common/LoadingBar';
+import useInfinteScroll from '../../hooks/useInfiniteScroll';
 
 interface Props {
   categoryCode: CategoryCode;
@@ -13,9 +14,18 @@ interface Props {
 
 const CommentSection = ({ categoryCode, feedId }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
 
   const { mutate: postComment } = usePostCommentMutation({ feedId });
-  const { data, isFetching, isLoading } = useCommentListQuery({ feedId });
+  const { data, isFetching, isLoading, hasNextPage, fetchNextPage } =
+    useCommentListQuery({ feedId });
+
+  useInfinteScroll({
+    targetEl: triggerRef.current,
+    hasMore: hasNextPage || false,
+    onIntersect: fetchNextPage,
+  });
+
   const comments = data?.pages.flatMap((page) => page.data) ?? [];
 
   const handleSubmit: FormEventHandler = async (e) => {
@@ -27,50 +37,46 @@ const CommentSection = ({ categoryCode, feedId }: Props) => {
 
   return (
     <div className="bg-[#ffedd5] flex flex-col w-[500px] h-[600px]">
-      {isFetching || isLoading ? (
-        <LoadingBar />
-      ) : (
-        <>
-          <div className="w-full h-[530px] overflow-y-auto p-[20px]">
-            {comments.length === 0 && (
-              <span className="text-gray-500 font-semibold">No Comment</span>
-            )}
-            {comments.map((comment) => (
-              <CommentItem
-                key={comment.commentId}
-                comment={comment}
-                categoryCode={categoryCode}
-                feedId={feedId}
-              />
-            ))}
-          </div>
-          <div className="h-[70px] flex flex-row justify-between items-center p-[20px]">
-            <div className="flex flex-row justify-center items-center gap-[5px]">
-              <FaCommentDots size={24} />
-              <span className="ml-2 text-gray-500 font-semibold">
-                {comments.length}
-              </span>
-            </div>
-            <form
-              onSubmit={handleSubmit}
-              className="flex items-center justify-center gap-[5px]"
-            >
-              <input
-                ref={inputRef}
-                type="text"
-                placeholder="댓글"
-                className="border border-solid border-gray-400 rounded-xl p-2 font-[Pretendard] w-[350px]"
-              />
-              <button
-                type="submit"
-                className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-sky-500 text-white h-[2.5rem] w-[2.5rem] text-sm font-semibold rounded-[12px]"
-              >
-                +
-              </button>
-            </form>
-          </div>
-        </>
-      )}
+      <div className="w-full h-[530px] overflow-y-auto p-[20px]">
+        {comments.length === 0 && (
+          <span className="text-gray-500 font-semibold">No Comment</span>
+        )}
+        {comments.map((comment) => (
+          <CommentItem
+            key={comment.commentId}
+            comment={comment}
+            categoryCode={categoryCode}
+            feedId={feedId}
+          />
+        ))}
+        {(isLoading || isFetching) && <LoadingBar />}
+        <div ref={triggerRef}></div>
+      </div>
+      <div className="h-[70px] flex flex-row justify-between items-center p-[20px]">
+        <div className="flex flex-row justify-center items-center gap-[5px]">
+          <FaCommentDots size={24} />
+          <span className="ml-2 text-gray-500 font-semibold">
+            {comments.length}
+          </span>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center justify-center gap-[5px]"
+        >
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="댓글"
+            className="border border-solid border-gray-400 rounded-xl p-2 font-[Pretendard] w-[350px]"
+          />
+          <button
+            type="submit"
+            className="hover:brightness-110 duration-300 cursor-pointer border border-solid bg-sky-500 text-white h-[2.5rem] w-[2.5rem] text-sm font-semibold rounded-[12px]"
+          >
+            +
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
