1f3c70f9eb775d4f9cea132298741d00131b9579
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-17 23:10:00 +0900

    💄 design : 기존 Map 페이지 배경 변경 및 피드별 배경 변경

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,8 +5,12 @@ export default {
     extend: {
       backgroundImage: {
         title: "url('/src/assets/bg/room.png')",
-        map: "url('/src/assets/bg/map.png')",
-        feed: "url('/src/assets/bg/village.png')",
+        map: "url('/src/assets/bg/map.gif')",
+        strFeed: "url('/src/assets/bg/strFeedBg.gif')",
+        dexFeed: "url('/src/assets/bg/dexFeedBg.gif')",
+        intFeed: "url('/src/assets/bg/intFeedBg.gif')",
+        charmFeed: "url('/src/assets/bg/charmFeedBg.gif')",
+        livingFeed: "url('/src/assets/bg/livingFeedBg.gif')",
         feedBox: "url('/src/assets/feed/feedItem.png')",
         board: "url('/src/assets/feed/board.png')",
         searchBar: "url('/src/assets/feed/searchBar.png')",
