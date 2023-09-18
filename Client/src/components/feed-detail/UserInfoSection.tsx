7e4e098babc4da53bf386822cfbc4f8bd52f181d
widrns15 <85780501+widrns15@users.noreply.github.com>
2023-09-19 08:17:26 +0900

    ✨ feat: 피드 & 피드 상세 페이지에서 특정 유저 프로필을 볼 수 있는 기능 추가

--- Client/src/components/feed-detail/UserInfoSection.tsx
+++ Client/src/components/feed-detail/UserInfoSection.tsx
@@ -3,22 +3,30 @@ import { FeedDetail } from '../../api/feed';
 import { CATEGORY_STATUS_MAP } from '../../utils/category';
 import { STATUS_ICON } from '../../utils/status';
 
+import { useNavigate } from 'react-router';
+
 interface Props {
   feedDetail: FeedDetail;
   categoryCode: CategoryCode;
 }
 
 const UserInfoSection = ({ feedDetail, categoryCode }: Props) => {
+  const nav = useNavigate();
   return (
     <div className="flex flex-col bg-orange-100  p-4 w-[200px]">
-      <div className="flex flex-col items-center p-4">
+      <div
+        className="flex flex-col items-center p-4 cursor-pointer"
+        onClick={() =>
+          nav(`/feed/${categoryCode}/profile/${feedDetail.nickname}`)
+        }
+      >
         <img
           src={feedDetail.profileImage}
           width={90}
           alt="profile pic"
-          className="mb-[8px] w-16 h-16 rounded-full object-cover "
+          className="mb-[8px] w-16 h-16 rounded-full object-cover"
         />
-        <span className="font-[Pretendard] font-semibold text-[20px] ">
+        <span className="font-[Pretendard] font-semibold text-[20px]">
           {feedDetail.nickname}
         </span>
         <div className="flex mt-1 items-center -ml-2">
@@ -32,7 +40,7 @@ const UserInfoSection = ({ feedDetail, categoryCode }: Props) => {
           </span>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-between py-2 "></div>
+      <div className="flex flex-col items-center justify-between py-2"></div>
     </div>
   );
 };
