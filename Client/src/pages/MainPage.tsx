3e4f4b67db74fb0685ebf1a83d24136e766d30dd
DongHun, Kim <donghun.kdh@gmail.com>
2023-09-19 20:45:36 +0900

    ✨ feat: 메인 페이지 가이드 추가; 메인 페이지 너비 설정

--- Client/src/pages/MainPage.tsx
+++ Client/src/pages/MainPage.tsx
@@ -5,7 +5,6 @@ import useCheckInQuery from '../hooks/useCheckInQuery';
 import { Navigate } from 'react-router';
 import { Steps } from 'intro.js-react';
 import 'intro.js/introjs.css';
-import guideButton from '../assets/common/guide-button.png';
 
 const MainPage = () => {
   const queryClient = useQueryClient();
@@ -15,13 +14,13 @@ const MainPage = () => {
     {
       element: '.statIcon',
       title: '모험의 시작 !',
-      intro: `책을 클릭해서 스탯을 확인하고, 스탯을 키우러 출발해볼까요~?`,
+      intro: `내 스테이터스를 확인하고,<br/>스테이터스를 쌓으러 떠날 수 있어요.`,
       position: 'left',
     },
     {
       element: '.profileIcon',
-      title: '정비는 언제나 환영이야',
-      intro: '닉네임 & 비밀번호 변경, 내가 쓴 피드를 확인할 수 있어요.',
+      title: '정비는 언제나 환영 !',
+      intro: '내 정보를 확인 및 수정하고,<br/>내가 쓴 피드를 확인할 수 있어요.',
       position: 'right',
     },
   ];
@@ -39,47 +38,56 @@ const MainPage = () => {
   }
 
   return (
-    <div className="w-full min-w-[1200px] h-screen flex justify-center items-center bg-[#3c0033] z-10">
+    <div className="w-full min-w-[1200px] h-screen flex justify-center items-center bg-[#3c0033] overflow-y-hidden">
       <div className="relative bg-title w-[1200px] h-[720px] bg-cover bg-no-repeat bg-center">
         {/* 책 - 스테이터스 */}
-        <Link to="/main/status">
-          <div className="absolute left-[410px] top-[360px] flex flex-col justify-between items-center gap-[16px] statIcon">
-            <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
-              My Status
-            </p>
+        <div className="absolute left-[410px] top-[360px] flex flex-col justify-between items-center gap-[16px] statIcon">
+          <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000] pointer-events-none">
+            My Status
+          </p>
+          <Link
+            onClick={() => {
+              setStepsEnabled(false);
+            }}
+            to="/main/status"
+          >
             <div className="w-[60px] h-[60px] bg-[url('/src/assets/objects/note.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"></div>
-          </div>
-        </Link>
+          </Link>
+        </div>
         {/* 종이 - 프로필 */}
-        <Link to="/main/profile">
-          <div className="absolute left-[755px] top-[240px] flex flex-col justify-between items-center gap-[16px] profileIcon">
-            <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
-              My Profile
-            </p>
+        <div className="absolute left-[755px] top-[240px] flex flex-col justify-between items-center gap-[16px] profileIcon">
+          <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000] pointer-events-none">
+            My Profile
+          </p>
+          <Link
+            onClick={() => {
+              setStepsEnabled(false);
+            }}
+            to="/main/profile"
+          >
             <div className="w-[50px] h-[60px] bg-[url('/src/assets/objects/paper.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"></div>
-          </div>
-        </Link>
+          </Link>
+        </div>
         {/* 문 - 로그아웃 */}
-        <Link to="/" onClick={handleLogout}>
-          <div className="absolute left-[562px] top-[540px] flex flex-col justify-between items-center gap-[16px]">
-            <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
-              Logout
-            </p>
+        <div className="absolute left-[562px] top-[540px] flex flex-col justify-between items-center gap-[16px]">
+          <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000] pointer-events-none">
+            Logout
+          </p>
+          <Link onClick={handleLogout} to="/">
             <div className="w-[63px] h-[99px] bg-[url('/src/assets/objects/door.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"></div>
-          </div>
-        </Link>
-        <Outlet />
+          </Link>
+        </div>
+        {/* 가이드 버튼 */}
         <div className="absolute left-[823px] top-[447px] flex flex-col justify-between items-center gap-[16px]">
-          <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000]">
+          <p className="text-[.8rem] text-white drop-shadow-[0_0_2px_#000] pointer-events-none">
             Guide
           </p>
-          <img
-            src={guideButton}
-            alt="guideButton"
+          <div
             onClick={() => setStepsEnabled(true)}
-            className=" w-10 cursor-pointer"
-          />
+            className="w-[50px] h-[50px] bg-[url('/src/assets/common/guide-button.png')] bg-cover bg-no-repeat bg-cneter animate-[scale_1s_linear_alternate_infinite] duration-[.3s] hover:animate-none hover:brightness-125 hover:scale-105 cursor-pointer"
+          ></div>
         </div>
+        <Outlet />
       </div>
       <Steps
         enabled={stepsEnabled}
