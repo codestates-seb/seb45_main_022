a722868f909a7e60866984c74b06a10bf35b1446
Kangs <toaxjakf@gmail.com>
2023-09-19 09:24:23 +0900

    🐛 fix: 해시태그 검색시 포함이 아닌 일치하는 해시태그만 나오게 수정

--- Server/src/main/java/com/codestatus/domain/hashTag/repository/HashTagRepository.java
+++ Server/src/main/java/com/codestatus/domain/hashTag/repository/HashTagRepository.java
@@ -14,7 +14,7 @@ public interface HashTagRepository extends JpaRepository <HashTag, Long> {
     @Query("SELECT h FROM HashTag h WHERE h.body = :body")
     Optional<HashTag> findHashTagByBody(@Param("body") String body);
 
-    @Query("SELECT h FROM HashTag h WHERE h.body LIKE %:body%")
+    @Query("SELECT h FROM HashTag h WHERE h.body = :body")
     Page<HashTag> findHashTagByBodyLike(@Param("body") String body, Pageable pageable);
 
 }
