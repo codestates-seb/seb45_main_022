61d5b210c83d0debbf083e1c685a483ea5921aa7
Jiwon Lee <giwon.yi339@gmail.com>
2023-09-22 15:55:23 +0900

    ✨feat : oAuth2 기능 구현

--- Client/src/components/auth/GoogleLoginButton.tsx
+++ Client/src/components/auth/GoogleLoginButton.tsx
@@ -8,7 +8,7 @@ const GoogleLoginButton = () => {
       onClick={handleGoogleLogin}
       className="w-[200px] h-[50px] bg-yellow-300 rounded hover:brightness-110 duration-300 cursor-pointer text-sm border-solid border-black "
     >
-      Kakao Login
+      Google Login
     </button>
   );
 };
